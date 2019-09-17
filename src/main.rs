43cba48610491a0dc192703d5c96dfdb8565448e
Rustin-Liu <rustin.liu@gmail.com>
2019-09-17 22:36:06 +0800

    post 1: use no-std

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
-#![no_std]
-fn main() {
-    println!("Hello, world!");
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
