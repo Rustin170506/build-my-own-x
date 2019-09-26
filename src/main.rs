9d3cf4de0a135bb9d388abba1bee5d9223d820bb
Rustin-Liu <rustin.liu@gmail.com>
2019-09-26 22:27:48 +0800

    post 2: print hello world

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // don't mangle the name of this function
+static HELLO: &[u8] = b"Hi Rustin, I am Samantha!";
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
