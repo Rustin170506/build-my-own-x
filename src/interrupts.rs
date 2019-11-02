5798eba786d598686da90c4203494c8c1556f166
Rustin-Liu <rustin.liu@gmail.com>
2019-11-02 18:05:43 +0800

    pass all test

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -19,7 +19,8 @@ extern "x86-interrupt" fn double_fault_handler(
     stack_frame: &mut InterruptStackFrame,
     _error_code: u64,
 ) {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    println!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    loop {}
 }
 
 pub fn init_idt() {
