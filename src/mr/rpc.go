1d76baf9cabe5b180a26f98bd48f837ee00dfc17
Rustin-Liu <rustin.liu@gmail.com>
2020-01-30 21:42:49 +0800

    init project

---
+++ src/mr/rpc.go
@@ -0,0 +1,23 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type ExampleArgs struct {
+	X int
+}
+
+type ExampleReply struct {
+	Y int
+}
+
+// Add your RPC definitions here.
+
