f5005e3b20606b4ba1f33dc4fc1302043793b043
hi-rustin <rustin.liu@gmail.com>
2022-06-19 18:08:42 +0800

    rq: add test_boolean_field_csv_data_source
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- rq/src/datasource/csv_data_source.rs
+++ rq/src/datasource/csv_data_source.rs
@@ -5,11 +5,11 @@ use crate::datatypes::{
 };
 use anyhow::{Ok, Result};
 use arrow::{
-    array::{Array, BooleanArray},
-    datatypes::{DataType, Field as ArrowField, Schema as ArrowSchema},
+    array::BooleanArray,
+    datatypes::{DataType, Schema as ArrowSchema},
 };
-use csv::StringRecord;
-use std::{fs::File, rc::Rc, slice::Iter};
+use csv::{Reader, ReaderBuilder, StringRecord};
+use std::{fs::File, rc::Rc};
 
 struct CsvDataSource {
     file_name: String,
@@ -30,118 +30,113 @@ impl DataSource for CsvDataSource {
         } else {
             self.schema.select(projections)
         };
-        let mut csv_reader_builder = csv::ReaderBuilder::new();
+        let mut csv_reader_builder = ReaderBuilder::new();
         csv_reader_builder.has_headers(self.has_headers);
         let mut csv_reader = csv_reader_builder.from_reader(file);
         csv_reader.set_headers(schema.fields.iter().map(|f| f.name.clone()).collect());
-        Ok(Box::new(
-            ReaderIterator::new(csv_reader, schema, self.batch_size).into_iter(),
-        ))
+        let csv_data_source_reader = CsvDataSourceReader::new(csv_reader, schema, self.batch_size);
+        Ok(Box::new(csv_data_source_reader))
     }
 }
 
-struct ReaderIterator {
-    schema: Schema,
-    parser: csv::Reader<File>,
-    batch_size: usize,
-    next_batch: Option<RecordBatch>,
-    started: bool,
+impl CsvDataSource {
+    fn new(file_name: String, schema: Schema, has_headers: bool, batch_size: usize) -> Self {
+        Self {
+            file_name,
+            schema,
+            has_headers,
+            batch_size,
+        }
+    }
 }
 
-impl Iterator for ReaderIterator {
+struct CsvDataSourceReader {
+    schema: Schema,
+    parser: Reader<File>,
+    batch_size: usize,
+}
+
+impl Iterator for CsvDataSourceReader {
     type Item = RecordBatch;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if !self.started {
-            self.has_next();
-        }
-
-        self.next_batch.clone()
+        self.next_batch()
     }
 }
 
-impl ReaderIterator {
-    fn new(parser: csv::Reader<File>, schema: Schema, batch_size: usize) -> ReaderIterator {
-        ReaderIterator {
+impl CsvDataSourceReader {
+    fn new(parser: Reader<File>, schema: Schema, batch_size: usize) -> CsvDataSourceReader {
+        CsvDataSourceReader {
             schema,
             parser,
             batch_size,
-            next_batch: None,
-            started: false,
         }
-    }
-
-    fn has_next(&mut self) -> bool {
-        if !self.started {
-            self.started = true;
-            self.next_batch = self.next_batch();
-        }
-
-        self.next_batch.is_some()
     }
 
     fn next_batch(&mut self) -> Option<RecordBatch> {
         let mut records = Vec::with_capacity(self.batch_size);
+        // Skip headers.
+        let _ = self.parser.records().next();
         loop {
             let line = self.parser.records().next();
-            if line.is_none() {
+            if let Some(line) = line {
+                records.push(line.unwrap());
+            } else {
                 break;
-            }
-
-            if line.is_some() {
-                let line = line.unwrap().unwrap();
-                records.push(line);
             }
 
             if records.len() >= self.batch_size {
                 break;
             }
-
-            if records.is_empty() {
-                return None;
-            }
         }
+
+        if records.is_empty() {
+            return None;
+        }
+
         Some(self.create_batch(records))
     }
 
     fn create_batch(&mut self, rows: Vec<StringRecord>) -> RecordBatch {
-        let mut schema: ArrowSchema = self.schema.clone().into();
-        let arrays: Vec<Box<dyn Array>> = schema
+        let schema: ArrowSchema = self.schema.clone().into();
+        let arrays = schema
             .fields()
             .iter()
             .enumerate()
             .map(|(col_index, field)| match field.data_type() {
-                DataType::Boolean => Box::new(
-                    rows.iter()
-                        .map(|row| match row.get(col_index) {
-                            Some(s) => {
-                                if s.is_empty() {
-                                    return None;
-                                }
-
-                                let parsed = parse_bool(s);
-                                match parsed {
-                                    Some(e) => Some(e),
-                                    None => panic!("Failed to parse bool: {}", s),
-                                }
-                            }
-                            None => None,
-                        })
-                        .collect::<BooleanArray>(),
-                ) as Box<dyn Array>,
-                _ => panic!("Unsupported type"),
+                DataType::Boolean => build_boolean_array(rows.clone(), col_index),
+                _ => unreachable!(),
             })
             .collect();
 
-        let fields = arrays
-            .into_iter()
-            .map(|a| Rc::new(ArrowFieldArray::new(a)) as ArrayRef)
-            .collect();
         RecordBatch {
             schema: self.schema.clone(),
-            fields: fields,
+            fields: arrays,
         }
     }
+}
+
+fn build_boolean_array(rows: Vec<StringRecord>, col_index: usize) -> ArrayRef {
+    let array = Box::new(
+        rows.iter()
+            .map(|row| match row.get(col_index) {
+                Some(s) => {
+                    if s.is_empty() {
+                        return None;
+                    }
+
+                    let parsed = parse_bool(s);
+                    match parsed {
+                        Some(e) => Some(e),
+                        None => panic!("Failed to parse bool: {}", s),
+                    }
+                }
+                None => None,
+            })
+            .collect::<BooleanArray>(),
+    );
+
+    Rc::new(ArrowFieldArray::new(array)) as ArrayRef
 }
 
 fn parse_bool(string: &str) -> Option<bool> {
@@ -151,5 +146,49 @@ fn parse_bool(string: &str) -> Option<bool> {
         Some(true)
     } else {
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::datatypes::schema::Field;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_boolean_field_csv_data_source() {
+        let mut data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        data_path.push("tests/data/boolean_field.csv");
+        let schema = Schema::new(vec![Field::new("c1".to_string(), DataType::Boolean)]);
+        let csv_data_source = CsvDataSource::new(
+            data_path.into_os_string().into_string().unwrap(),
+            schema,
+            false,
+            3,
+        );
+        let mut reader = csv_data_source.scan(vec!["c1".to_string()]).unwrap();
+        let batch = reader.next().unwrap();
+
+        assert_eq!(batch.row_count(), 3);
+        assert_eq!(batch.column_count(), 1);
+        assert_eq!(batch.field(0).get_type(), DataType::Boolean);
+        assert!(batch
+            .field(0)
+            .get_value(0)
+            .unwrap()
+            .downcast_ref::<bool>()
+            .unwrap());
+        assert!(!batch
+            .field(0)
+            .get_value(1)
+            .unwrap()
+            .downcast_ref::<bool>()
+            .unwrap());
+        assert!(batch
+            .field(0)
+            .get_value(2)
+            .unwrap()
+            .downcast_ref::<bool>()
+            .unwrap());
     }
 }
