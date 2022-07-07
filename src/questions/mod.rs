2ed71f1076c57f6fb21e4e1693ea10c0b55de1a2
hi-rustin <rustin.liu@gmail.com>
2022-07-07 22:19:15 +0800

    lr: is_palindrome
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/questions/mod.rs
+++ src/questions/mod.rs
@@ -1,1 +1,2 @@
-mod q001_tow_sum;
+mod q_001_tow_sum;
+mod q_009_palindrome_number;
---
+++ src/questions/q_009_palindrome_number.rs
@@ -0,0 +1,27 @@
+pub fn is_palindrome(x: i32) -> bool {
+    if x < 0 || (x % 10 == 0 && x != 0) {
+        return false;
+    }
+    let mut x = x;
+    let mut reverted_num = 0;
+
+    while x > reverted_num {
+        reverted_num = reverted_num * 10 + x % 10;
+        x = x / 10;
+    }
+
+    reverted_num == x || reverted_num / 10 == x
+}
+
+#[cfg(test)]
+mod tests {
+    use super::is_palindrome;
+
+    #[test]
+    fn test_is_palindrome() {
+        assert!(is_palindrome(121));
+        assert!(!is_palindrome(-121));
+        assert!(!is_palindrome(10));
+        assert!(is_palindrome(1221));
+    }
+}
