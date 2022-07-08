826c5db11a8cd1cfa4d2b3fd5c529352c965e2a7
hi-rustin <rustin.liu@gmail.com>
2022-07-08 17:27:23 +0800

    rq: add tests for exprs
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- rq/src/physical_plan/expr.rs
+++ rq/src/physical_plan/expr.rs
@@ -44,6 +44,12 @@ pub(crate) struct Column {
     pub(crate) i: usize,
 }
 
+impl Column {
+    pub(crate) fn new(i: usize) -> Self {
+        Self { i }
+    }
+}
+
 impl PhysicalExpr for Column {
     fn evaluate(&self, input: &RecordBatch) -> Result<ArrayRef> {
         Ok(input.field(self.i).clone())
@@ -102,6 +108,8 @@ impl ToString for ScalarValue {
     }
 }
 
+/// For binary expressions we need to evaluate the left and right input expressions
+/// and then evaluate the specific binary operator against those input values.
 pub(crate) struct BinaryExpr {
     pub(crate) op: Operator,
     pub(crate) left: Box<Expr>,
@@ -114,28 +122,32 @@ impl PhysicalExpr for BinaryExpr {
         let right = self.right.evaluate(input)?;
         assert!(left.get_type() == right.get_type());
         let arrow_type = left.get_type();
-        let mut array = vec![];
+        let mut vals = vec![];
         match self.op {
             Operator::Add => {
                 for i in 0..left.size() {
-                    let value = add(left.get_value(i)?, right.get_value(i)?, left.get_type());
-                    array.push(value);
+                    let value = add(&left.get_value(i)?, &right.get_value(i)?, &arrow_type);
+                    vals.push(value);
                 }
             }
             _ => unimplemented!(),
         }
 
-        build_from_values(&array, arrow_type)
+        evaluate_from_values(&vals, &arrow_type)
     }
 }
 
 impl ToString for BinaryExpr {
     fn to_string(&self) -> String {
-        todo!()
+        match self.op {
+            Operator::Add => format!("{} + {}", self.left.to_string(), self.right.to_string()),
+            _ => unimplemented!(),
+        }
     }
 }
 
-fn build_from_values(array: &[Box<dyn Any>], data_type: DataType) -> Result<ArrayRef> {
+// Build the arrow array from the values.
+fn evaluate_from_values(array: &[Box<dyn Any>], data_type: &DataType) -> Result<ArrayRef> {
     match data_type {
         DataType::Int64 => {
             let arrow_array = Int64Array::from(
@@ -146,11 +158,29 @@ fn build_from_values(array: &[Box<dyn Any>], data_type: DataType) -> Result<Arra
             );
             Ok(Rc::new(ArrowFieldArray::new(Box::new(arrow_array))))
         }
-        _ => unimplemented!(),
+        DataType::Float32 => {
+            let arrow_array = arrow::array::Float32Array::from(
+                array
+                    .iter()
+                    .map(|v| *v.downcast_ref::<f32>().unwrap())
+                    .collect::<Vec<f32>>(),
+            );
+            Ok(Rc::new(ArrowFieldArray::new(Box::new(arrow_array))))
+        }
+        DataType::Float64 => {
+            let arrow_array = arrow::array::Float64Array::from(
+                array
+                    .iter()
+                    .map(|v| *v.downcast_ref::<f64>().unwrap())
+                    .collect::<Vec<f64>>(),
+            );
+            Ok(Rc::new(ArrowFieldArray::new(Box::new(arrow_array))))
+        }
+        _ => unreachable!(),
     }
 }
 
-fn add(l: Box<dyn Any>, r: Box<dyn Any>, data_type: DataType) -> Box<dyn Any> {
+fn add(l: &dyn Any, r: &dyn Any, data_type: &DataType) -> Box<dyn Any> {
     match data_type {
         DataType::Int64 => {
             let l = l.downcast_ref::<i64>().unwrap();
@@ -167,6 +197,69 @@ fn add(l: Box<dyn Any>, r: Box<dyn Any>, data_type: DataType) -> Box<dyn Any> {
             let r = r.downcast_ref::<f64>().unwrap();
             Box::new(*l + *r)
         }
-        _ => unimplemented!(),
+        _ => unreachable!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Column, PhysicalExpr, ScalarValue};
+    use crate::data_types::{
+        arrow_field_array::ArrowFieldArray,
+        column_array::ArrayRef,
+        record_batch::RecordBatch,
+        schema::{Field, Schema},
+    };
+    use arrow::{array::Int64Array, datatypes::DataType};
+    use std::rc::Rc;
+
+    #[test]
+    fn test_column_expr_evaluate() {
+        let id = Int64Array::from(vec![1, 2, 3, 4, 5]);
+        let id_arrary = vec![Rc::new(ArrowFieldArray::new(Box::new(id))) as ArrayRef];
+        let schema = Schema::new(vec![Field::new("id".to_string(), DataType::Int32)]);
+        let input = RecordBatch::new(schema, id_arrary);
+        let expr = Column::new(0);
+        assert!(expr.evaluate(&input).is_ok());
+        assert!(
+            expr.evaluate(&input)
+                .unwrap()
+                .get_value(0)
+                .unwrap()
+                .downcast_ref::<i64>()
+                .unwrap()
+                == &1
+        );
+    }
+
+    #[test]
+    fn test_column_expr_to_string() {
+        let expr = Column::new(0);
+        assert_eq!(expr.to_string(), "#0");
+    }
+
+    #[test]
+    fn test_scalar_value_expr_evaluate() {
+        let id = Int64Array::from(vec![1]);
+        let id_arrary = vec![Rc::new(ArrowFieldArray::new(Box::new(id))) as ArrayRef];
+        let schema = Schema::new(vec![Field::new("id".to_string(), DataType::Int32)]);
+        let input = RecordBatch::new(schema, id_arrary);
+        let expr = ScalarValue::Int64(1);
+        assert!(expr.evaluate(&input).is_ok());
+        assert!(
+            expr.evaluate(&input)
+                .unwrap()
+                .get_value(0)
+                .unwrap()
+                .downcast_ref::<i64>()
+                .unwrap()
+                == &1
+        );
+    }
+
+    #[test]
+    fn test_scalar_value_expr_to_string() {
+        let expr = ScalarValue::Int64(1);
+        assert_eq!(expr.to_string(), "1");
     }
 }
