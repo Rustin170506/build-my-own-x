ea6c58fdee27be30e06385c06b89816725f0911c
hi-rustin <rustin.liu@gmail.com>
2022-07-12 11:44:08 +0800

    rq: not gteq expr
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- rq/src/physical_plan/expr.rs
+++ rq/src/physical_plan/expr.rs
@@ -212,6 +212,13 @@ impl PhysicalExpr for BinaryExpr {
                 }
                 evaluate_from_values(&vals, &DataType::Boolean)
             }
+            Operator::GtEq => {
+                for i in 0..left.size() {
+                    let value = gteq(&left.get_value(i)?, &right.get_value(i)?, &arrow_type);
+                    vals.push(value);
+                }
+                evaluate_from_values(&vals, &DataType::Boolean)
+            }
             _ => unimplemented!(),
         }
     }
@@ -232,6 +239,7 @@ impl ToString for BinaryExpr {
             Operator::Lt => format!("{} < {}", self.left.to_string(), self.right.to_string()),
             Operator::LtEq => format!("{} <= {}", self.left.to_string(), self.right.to_string()),
             Operator::Gt => format!("{} > {}", self.left.to_string(), self.right.to_string()),
+            Operator::GtEq => format!("{} >= {}", self.left.to_string(), self.right.to_string()),
             _ => unimplemented!(),
         }
     }
@@ -533,6 +541,31 @@ fn gt(l: &Box<dyn Any>, r: &Box<dyn Any>, data_type: &DataType) -> Box<dyn Any> 
             let l = OrderedFloat(*l);
             let r = OrderedFloat(*r);
             Box::new(l > r)
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn gteq(l: &Box<dyn Any>, r: &Box<dyn Any>, data_type: &DataType) -> Box<dyn Any> {
+    match data_type {
+        DataType::Int64 => {
+            let l = l.downcast_ref::<i64>().unwrap();
+            let r = r.downcast_ref::<i64>().unwrap();
+            Box::new(l >= r)
+        }
+        DataType::Float32 => {
+            let l = l.downcast_ref::<f32>().unwrap();
+            let r = r.downcast_ref::<f32>().unwrap();
+            let l = OrderedFloat(*l);
+            let r = OrderedFloat(*r);
+            Box::new(l >= r)
+        }
+        DataType::Float64 => {
+            let l = l.downcast_ref::<f64>().unwrap();
+            let r = r.downcast_ref::<f64>().unwrap();
+            let l = OrderedFloat(*l);
+            let r = OrderedFloat(*r);
+            Box::new(l >= r)
         }
         _ => unreachable!(),
     }
@@ -1000,5 +1033,38 @@ mod tests {
             Box::new(Expr::Literal(ScalarValue::Int64(1))),
         );
         assert_eq!(expr.to_string(), "#0 > 1");
+    }
+
+    #[test]
+    fn test_gt_eq_expr_evaluate() {
+        let id = Int64Array::from(vec![2]);
+        let id_arrary = vec![Rc::new(ArrowFieldArray::new(Box::new(id))) as ArrayRef];
+        let schema = Schema::new(vec![Field::new("id".to_string(), DataType::Int64)]);
+        let input = RecordBatch::new(schema, id_arrary);
+        let expr = BinaryExpr::new(
+            Operator::GtEq,
+            Box::new(Expr::Column(Column::new(0))),
+            Box::new(Expr::Literal(ScalarValue::Int64(2))),
+        );
+        assert!(expr.evaluate(&input).is_ok());
+        assert!(
+            expr.evaluate(&input)
+                .unwrap()
+                .get_value(0)
+                .unwrap()
+                .downcast_ref::<bool>()
+                .unwrap()
+                == &true
+        );
+    }
+
+    #[test]
+    fn test_gt_eq_expr_to_string() {
+        let expr = BinaryExpr::new(
+            Operator::GtEq,
+            Box::new(Expr::Column(Column::new(0))),
+            Box::new(Expr::Literal(ScalarValue::Int64(2))),
+        );
+        assert_eq!(expr.to_string(), "#0 >= 2");
     }
 }
