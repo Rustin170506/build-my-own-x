f8a46185b7ed30fc681c8ca9a9fa1cd146edb89b
hi-rustin <rustin.liu@gmail.com>
2022-07-15 22:38:36 +0800

    rq: add AggregateExpr and add tests
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- rq/src/lib.rs
+++ rq/src/lib.rs
@@ -1,7 +1,6 @@
 // FIXME: This is a temporary solution to get the tests working.
 #![allow(dead_code)]
 #![allow(unused)]
-#![feature(const_type_id)]
 pub(crate) mod data_source;
 pub(crate) mod data_types;
 pub(crate) mod logical_plan;
