7f3578ba055eedd411937043a1ac33b9392ab84d
hi-rustin <rustin.liu@gmail.com>
2022-07-17 17:47:39 +0800

    rq: add Projection plan
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- rq/src/physical_plan/plan.rs
+++ rq/src/physical_plan/plan.rs
@@ -6,8 +6,10 @@ use crate::{
 use anyhow::Result;
 use std::fmt::Display;
 
+use super::scan::Scan;
+
 /// A physical plan represents an executable piece of code that will produce data.
-trait PhysicalPlan: Display {
+pub(crate) trait PhysicalPlan: Display {
     /// Return the schema.
     fn schema(&self) -> Schema;
 
@@ -62,73 +64,5 @@ impl Display for Plan {
         match self {
             Plan::Scan(scan) => scan.fmt(f),
         }
-    }
-}
-
-// Scan a data source with optional push-down projection.
-pub(crate) struct Scan {
-    data_source: Source,
-    projection: Vec<String>,
-}
-
-impl Scan {
-    pub(crate) fn new(data_source: Source, projection: Vec<String>) -> Self {
-        Scan {
-            data_source,
-            projection,
-        }
-    }
-}
-
-impl PhysicalPlan for Scan {
-    fn schema(&self) -> Schema {
-        self.data_source
-            .get_schema()
-            .select(self.projection.iter().map(|s| s.as_str()).collect())
-    }
-
-    fn execute(&self) -> Result<Box<dyn Iterator<Item = RecordBatch> + '_>> {
-        self.data_source
-            .scan(self.projection.iter().map(|s| s.as_str()).collect())
-    }
-
-    fn children(&self) -> Vec<&Plan> {
-        vec![]
-    }
-}
-
-impl Display for Scan {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "ScanExec: projection={}",
-            self.projection
-                .iter()
-                .map(|x| x.to_string())
-                .collect::<Vec<String>>()
-                .join(",")
-        )
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::Scan;
-    use crate::{
-        data_source::{csv_data_source::CsvDataSource, DataSource, Source},
-        data_types::schema::{Field, Schema},
-    };
-    use arrow::datatypes::DataType;
-    use std::path::PathBuf;
-
-    #[test]
-    fn test_scan_display() {
-        let mut data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        data_path.push("tests/data/boolean_field.csv");
-        let schema = Schema::new(vec![Field::new("c1".to_string(), DataType::Boolean)]);
-        let csv_data_source =
-            CsvDataSource::new(data_path.into_os_string().into_string().unwrap(), schema, 3);
-        let scan = Scan::new(Source::Csv(csv_data_source), vec!["c1".to_string()]);
-        assert_eq!(scan.to_string(), "ScanExec: projection=c1");
     }
 }
---
+++ rq/src/physical_plan/scan.rs
@@ -0,0 +1,75 @@
+use super::plan::{PhysicalPlan, Plan};
+use crate::{
+    data_source::{DataSource, Source},
+    data_types::{record_batch::RecordBatch, schema::Schema},
+};
+use anyhow::Result;
+use std::fmt::Display;
+
+// Scan a data source with optional push-down projection.
+pub(crate) struct Scan {
+    data_source: Source,
+    projection: Vec<String>,
+}
+
+impl Scan {
+    pub(crate) fn new(data_source: Source, projection: Vec<String>) -> Self {
+        Scan {
+            data_source,
+            projection,
+        }
+    }
+}
+
+impl PhysicalPlan for Scan {
+    fn schema(&self) -> Schema {
+        self.data_source
+            .get_schema()
+            .select(self.projection.iter().map(|s| s.as_str()).collect())
+    }
+
+    fn execute(&self) -> Result<Box<dyn Iterator<Item = RecordBatch> + '_>> {
+        self.data_source
+            .scan(self.projection.iter().map(|s| s.as_str()).collect())
+    }
+
+    fn children(&self) -> Vec<&Plan> {
+        vec![]
+    }
+}
+
+impl Display for Scan {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "ScanExec: projection={}",
+            self.projection
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join(",")
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Scan;
+    use crate::{
+        data_source::{csv_data_source::CsvDataSource, DataSource, Source},
+        data_types::schema::{Field, Schema},
+    };
+    use arrow::datatypes::DataType;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_scan_display() {
+        let mut data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        data_path.push("tests/data/boolean_field.csv");
+        let schema = Schema::new(vec![Field::new("c1".to_string(), DataType::Boolean)]);
+        let csv_data_source =
+            CsvDataSource::new(data_path.into_os_string().into_string().unwrap(), schema, 3);
+        let scan = Scan::new(Source::Csv(csv_data_source), vec!["c1".to_string()]);
+        assert_eq!(scan.to_string(), "ScanExec: projection=c1");
+    }
+}
