2156ca548e89c95b57b28e3022fefffee17145d3
hi-rustin <rustin.liu@gmail.com>
2022-08-11 11:11:00 +0800

    rq: fix csv source bug
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- rq/src/data_source/csv_data_source.rs
+++ rq/src/data_source/csv_data_source.rs
@@ -1,24 +1,26 @@
+use std::{fs::File, rc::Rc};
+
 use super::{reader_parser::Parser, DataSource};
 use crate::data_types::{
     arrow_field_array::ArrowFieldArray, column_array::ArrayRef, record_batch::RecordBatch,
     schema::Schema,
 };
+
 use anyhow::{Ok, Result};
 use arrow::{
     array::{BooleanArray, PrimitiveArray, StringArray},
     datatypes::{
-        ArrowPrimitiveType, DataType as ArrowDataType, Float32Type, Float64Type, Int16Type,
+        ArrowPrimitiveType, DataType as ArrowDataType, Field, Float32Type, Float64Type, Int16Type,
         Int32Type, Int64Type, Int8Type, Schema as ArrowSchema, UInt16Type, UInt32Type, UInt64Type,
         UInt8Type,
     },
 };
 use csv::{Reader, ReaderBuilder, StringRecord};
-use std::{fs::File, rc::Rc};
 
 // A data source that reads from a CSV file.
 #[derive(Clone)]
 pub(crate) struct CsvDataSource {
-    file_name: String,
+    file_path: String,
     schema: Schema,
     // The total number of rows in the CSV file.
     batch_size: usize,
@@ -30,19 +32,24 @@ impl DataSource for CsvDataSource {
     }
 
     fn scan(&self, projections: Vec<&str>) -> Result<Box<dyn Iterator<Item = RecordBatch>>> {
-        let file = File::open(&self.file_name)?;
-        let schema = if projections.is_empty() {
-            self.schema.clone()
-        } else {
-            self.schema.select(projections)
-        };
+        let file = File::open(&self.file_path)?;
+
         let mut csv_reader_builder = ReaderBuilder::new();
         csv_reader_builder.has_headers(false);
         let mut csv_reader = csv_reader_builder.from_reader(file);
         // Set headers for the CSV reader.
         // This will append the name into the first record of reader.
-        csv_reader.set_headers(schema.fields.iter().map(|f| f.name.clone()).collect());
+        // We have to set all the fields of the schema to be able to parse the CSV file.
+        // Otherwise, the CSV reader will not be able to get the right index for projection.
+        csv_reader.set_headers(self.schema.fields.iter().map(|f| f.name.clone()).collect());
+
+        let schema = if projections.is_empty() {
+            self.schema.clone()
+        } else {
+            self.schema.select(projections)
+        };
         let csv_data_source_reader = CsvDataSourceReader::new(csv_reader, schema, self.batch_size);
+
         Ok(Box::new(csv_data_source_reader))
     }
 }
@@ -50,7 +57,7 @@ impl DataSource for CsvDataSource {
 impl CsvDataSource {
     pub(crate) fn new(file_name: String, schema: Schema, batch_size: usize) -> Self {
         Self {
-            file_name,
+            file_path: file_name,
             schema,
             batch_size,
         }
@@ -59,8 +66,8 @@ impl CsvDataSource {
 
 // A reader for the CSV data source with the specified schema.
 struct CsvDataSourceReader {
-    schema: Schema,
     parser: Reader<File>,
+    schema: Schema,
     batch_size: usize,
 }
 
@@ -74,17 +81,18 @@ impl Iterator for CsvDataSourceReader {
 
 impl CsvDataSourceReader {
     fn new(parser: Reader<File>, schema: Schema, batch_size: usize) -> CsvDataSourceReader {
-        CsvDataSourceReader {
-            schema,
+        let mut reader = CsvDataSourceReader {
             parser,
+            schema,
             batch_size,
-        }
+        };
+        // Skip the header of the CSV file.
+        let _ = reader.parser.records().next();
+        reader
     }
 
     fn next_batch(&mut self) -> Option<RecordBatch> {
         let mut records = Vec::with_capacity(self.batch_size);
-        // Skip headers.
-        let _ = self.parser.records().next();
         loop {
             let line = self.parser.records().next();
             if let Some(line) = line {
@@ -109,17 +117,30 @@ impl CsvDataSourceReader {
     // String -> ArrowFieldArray -> ArrayRef -> RecordBatch.
     fn create_batch(&mut self, rows: Vec<StringRecord>) -> RecordBatch {
         let schema: ArrowSchema = self.schema.clone().into();
-        let arrays = schema
+        let filed_with_col_index: Vec<(usize, &Field)> = schema
             .fields()
             .iter()
-            .enumerate()
+            .map(|f| {
+                let col_index = self
+                    .parser
+                    .headers()
+                    .unwrap()
+                    .iter()
+                    .position(|h| h == f.name())
+                    .unwrap();
+                (col_index, f)
+            })
+            .collect();
+
+        let arrays = filed_with_col_index
+            .iter()
             .map(|(col_index, field)| match field.data_type() {
-                ArrowDataType::Boolean => build_boolean_array(&rows, col_index),
-                ArrowDataType::Int32 => build_primitive_array::<Int32Type>(&rows, col_index),
-                ArrowDataType::Int64 => build_primitive_array::<Int64Type>(&rows, col_index),
-                ArrowDataType::Float32 => build_primitive_array::<Float32Type>(&rows, col_index),
-                ArrowDataType::Float64 => build_primitive_array::<Float64Type>(&rows, col_index),
-                ArrowDataType::Utf8 => build_string_array(&rows, col_index),
+                ArrowDataType::Boolean => build_boolean_array(&rows, *col_index),
+                ArrowDataType::Int32 => build_primitive_array::<Int32Type>(&rows, *col_index),
+                ArrowDataType::Int64 => build_primitive_array::<Int64Type>(&rows, *col_index),
+                ArrowDataType::Float32 => build_primitive_array::<Float32Type>(&rows, *col_index),
+                ArrowDataType::Float64 => build_primitive_array::<Float64Type>(&rows, *col_index),
+                ArrowDataType::Utf8 => build_string_array(&rows, *col_index),
                 _ => unreachable!(),
             })
             .collect();
@@ -202,9 +223,31 @@ fn build_string_array(rows: &[StringRecord], col_index: usize) -> ArrayRef {
 
 #[cfg(test)]
 mod tests {
+    use std::{any::Any, fmt::Debug, path::PathBuf};
+
     use super::*;
     use crate::data_types::{column_array::DataType, schema::Field};
-    use std::{any::Any, fmt::Debug, path::PathBuf};
+
+    fn assert_type_and_values<T: Any + PartialEq + Debug>(
+        batch: &RecordBatch,
+        index: usize,
+        data_type: DataType,
+        values: Vec<T>,
+    ) {
+        assert_eq!(batch.field(index).get_type(), data_type);
+
+        for (idx, val) in values.iter().enumerate() {
+            assert_eq!(
+                batch
+                    .field(index)
+                    .get_value(idx)
+                    .unwrap()
+                    .downcast_ref::<T>()
+                    .unwrap(),
+                val
+            );
+        }
+    }
 
     #[test]
     fn test_boolean_field_csv_data_source() {
@@ -260,27 +303,6 @@ mod tests {
 
         assert_eq!(batch.row_count(), 3);
         assert_eq!(batch.column_count(), 6);
-
-        fn assert_type_and_values<T: Any + PartialEq + Debug>(
-            batch: &RecordBatch,
-            index: usize,
-            data_type: DataType,
-            values: Vec<T>,
-        ) {
-            assert_eq!(batch.field(index).get_type(), data_type);
-
-            for (idx, val) in values.iter().enumerate() {
-                assert_eq!(
-                    batch
-                        .field(index)
-                        .get_value(idx)
-                        .unwrap()
-                        .downcast_ref::<T>()
-                        .unwrap(),
-                    val
-                );
-            }
-        }
 
         assert_type_and_values::<i32>(&batch, 0, DataType::Int32, vec![1, 2, 3]);
         assert_type_and_values::<i32>(&batch, 1, DataType::Int32, vec![9, 10, 11]);
@@ -338,6 +360,40 @@ mod tests {
                 .downcast_ref::<String>()
                 .unwrap(),
             "c"
+        );
+    }
+
+    #[test]
+    fn test_field_with_projection() {
+        let mut data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        data_path.push("tests/data/primitive_field.csv");
+        let schema = Schema::new(vec![
+            Field::new("c1".to_string(), DataType::Int32),
+            Field::new("c2".to_string(), DataType::Int32),
+            Field::new("c3".to_string(), DataType::Int64),
+            Field::new("c4".to_string(), DataType::Int64),
+            Field::new("c5".to_string(), DataType::Float32),
+            Field::new("c6".to_string(), DataType::Float64),
+        ]);
+        let csv_data_source =
+            CsvDataSource::new(data_path.into_os_string().into_string().unwrap(), schema, 3);
+        let mut reader = csv_data_source.scan(vec!["c4", "c5", "c6"]).unwrap();
+        let batch = reader.next().unwrap();
+
+        assert_eq!(batch.row_count(), 3);
+        assert_eq!(batch.column_count(), 3);
+
+        assert_type_and_values::<i64>(&batch, 0, DataType::Int64, vec![30, 31, 32]);
+        assert_type_and_values::<f32>(&batch, 1, DataType::Float32, vec![1.0, 1.1, 1.2]);
+        assert_type_and_values::<f64>(
+            &batch,
+            2,
+            DataType::Float64,
+            vec![
+                0.0000000000000000000001,
+                0.0000000000000000000002,
+                0.0000000000000000000003,
+            ],
         );
     }
 }
