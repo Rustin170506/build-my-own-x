c966b6a9677917d3755d0d6befdfe40aa7b6d4c1
hi-rustin <rustin.liu@gmail.com>
2022-08-21 15:18:16 +0800

    lr: q_026
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/mod.rs
+++ lr/src/questions/mod.rs
@@ -2,5 +2,6 @@ mod q_001_tow_sum;
 mod q_009_palindrome_number;
 mod q_013_roman_to_integer;
 mod q_014_longest_common_prefix;
-mod q_20_valid_parentheses;
-mod q_21_merge_two_sorted_lists;
+mod q_020_valid_parentheses;
+mod q_021_merge_two_sorted_lists;
+mod q_026_remove_duplicates_from_sorted_array;
