c7c0db840071cce4426e864f4938f8375e67753a
hi-rustin <rustin.liu@gmail.com>
2022-12-02 09:56:44 +0800

    lr: q_875 uses i64
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/questions/q_875_koko_eating_bananas.rs
+++ src/questions/q_875_koko_eating_bananas.rs
@@ -1,23 +1,23 @@
 pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
-    let (mut left, mut right) = (1, *piles.iter().max().unwrap());
+    let (mut left, mut right) = (1_i64, *piles.iter().max().unwrap() as i64);
     let mut result = right;
 
     while left <= right {
         let mid = (left + right) / 2;
-        let mut hours = 0;
+        let mut hours = 0_i64;
         piles.iter().for_each(|p| {
-            hours += (p + mid - 1) / mid;
+            hours += ((*p as i64) + mid - 1) / mid;
         });
 
-        if hours <= h {
-            result = i32::min(result, mid);
+        if hours <= h as i64 {
+            result = i64::min(result, mid);
             right = mid - 1;
         } else {
             left = mid + 1
         }
     }
 
-    result
+    result as i32
 }
 
 #[cfg(test)]
@@ -29,5 +29,9 @@ mod tests {
         assert_eq!(min_eating_speed(vec![3, 6, 7, 11], 8), 4);
         assert_eq!(min_eating_speed(vec![30, 11, 23, 4, 20], 5), 30);
         assert_eq!(min_eating_speed(vec![30, 11, 23, 4, 20], 6), 23);
+        assert_eq!(
+            min_eating_speed(vec![805306368, 805306368, 805306368], 1000000000),
+            3
+        );
     }
 }
