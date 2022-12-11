4aae5a598abcf8174884472c793ac153fd225f7d
二手掉包工程师 <rustin.liu@gmail.com>
2022-12-11 12:35:53 +0000

    Add tun_tap dep
    Signed-off-by: 二手掉包工程师 <rustin.liu@gmail.com>

---
+++ tcp-rs/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cargo b --release
+sudo setcap cap_net_admin=eip target/release/tcp-rs
+target/release/tcp-rs &
+pid=$!
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
+wait $pid
