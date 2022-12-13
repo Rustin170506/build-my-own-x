87215efbbcae872ec341d6825bc90f0d0b92f228
二手掉包工程师 <rustin.liu@gmail.com>
2022-12-13 01:59:12 +0000

    Add etherparse
    Signed-off-by: 二手掉包工程师 <rustin.liu@gmail.com>

--- tcp-rs/run.sh
+++ tcp-rs/run.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cargo b --release
 sudo setcap cap_net_admin=eip target/release/tcp-rs
 target/release/tcp-rs &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT EXIT
 wait $pid
--- tcp-rs/src/main.rs
+++ tcp-rs/src/main.rs
@@ -3,9 +3,30 @@ use std::io;
 use tun_tap::Iface;
 
 fn main() -> io::Result<()> {
-    let nic = Iface::new("tun0",tun_tap::Mode::Tun)?;
-    let mut buf = [0u8;1504];
-    let nbytes = nic.recv(&mut buf)?;
-    eprint!("Received {} bytes: {:x?}",nbytes,&buf[..nbytes]);
-    Ok(())
+    let nic = Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    loop {
+        let nbytes = nic.recv(&mut buf)?;
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let protocol = p.protocol();
+                eprintln!(
+                    "{} -> {} {}b of protocol {}",
+                    src,
+                    dst,
+                    p.payload_len(),
+                    protocol
+                );
+            }
+            Err(e) => eprintln!("Ignoring weird packet {:?}", e),
+        }
+    }
 }
