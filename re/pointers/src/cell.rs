cb260206b911b6557046175ca070772da0252b86
hi-rustin <rustin.liu@gmail.com>
2023-01-10 22:04:20 +0800

    Add ref cell
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- re/pointers/src/cell.rs
+++ re/pointers/src/cell.rs
@@ -4,25 +4,27 @@ pub struct Cell<T> {
     value: UnsafeCell<T>,
 }
 
-
 impl<T> Cell<T> {
     pub fn new(value: T) -> Self {
-        Cell { value: UnsafeCell::new(value) }
+        Cell {
+            value: UnsafeCell::new(value),
+        }
     }
 
     pub fn set(&self, value: T) {
         // Safety: We know no-one else is concurrently mutating self.value(because !Sync).
         // Safety: We know we're not invalidating any references, because we never give any out.
-       unsafe {
-          *self.value.get()= value;
-       }
+        unsafe {
+            *self.value.get() = value;
+        }
     }
 
-    pub fn get(&self) -> T where T: Copy {
+    pub fn get(&self) -> T
+    where
+        T: Copy,
+    {
         // Safety: We know no-one else is modifying this value, since only this thread can mutate, and it
         // is executing this function instead.
-        unsafe {
-            *self.value.get()
-        }
+        unsafe { *self.value.get() }
     }
 }
