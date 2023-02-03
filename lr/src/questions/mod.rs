c8ab7a1d264412752c4266ed2e63032bdd0ef62f
hi-rustin <rustin.liu@gmail.com>
2023-02-03 13:51:33 +0800

    lr: q_297
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/mod.rs
+++ lr/src/questions/mod.rs
@@ -66,6 +66,7 @@ mod q_238_product_of_array_except_self;
 mod q_239_sliding_window_maximum;
 mod q_242_valid_anagram;
 mod q_287_find_the_duplicate_number;
+mod q_297_serialize_and_deserialize_binary_tree;
 mod q_347_top_k_frequent_elements;
 mod q_424_longest_repeating_character_replacement;
 mod q_543_diameter_of_binary_tree;
