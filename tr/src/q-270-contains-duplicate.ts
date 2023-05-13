593f73a9a371ec28a492212d4d4fba9b4df93cf8
hi-rustin <rustin.liu@gmail.com>
2023-05-14 00:01:52 +0800

    tr: Add more tests
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- tr/src/q-270-contains-duplicate.ts
+++ tr/src/q-270-contains-duplicate.ts
@@ -14,4 +14,5 @@ function containsDuplicate(nums: number[]): boolean {
 
 test('containsDuplicate', () => {
   expect(containsDuplicate([1, 2, 3, 1])).toBe(true)
+  expect(containsDuplicate([1, 2, 3, 4])).toBe(false)
 })
