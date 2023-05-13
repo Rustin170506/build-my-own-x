b8440fafd88b327dc37dbb10e90b6b93df20e9e6
hi-rustin <rustin.liu@gmail.com>
2023-05-14 00:07:39 +0800

    tr: add pre check
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- tr/src/q-270-contains-duplicate.ts
+++ tr/src/q-270-contains-duplicate.ts
@@ -1,6 +1,10 @@
 import { expect, test } from 'vitest'
 
 function containsDuplicate(nums: number[]): boolean {
+    if (nums.length == 1) {
+        return false;
+    }
+
     const set = new Set<number>();
     for (const num of nums) {
         if (set.has(num)) {
@@ -15,4 +19,5 @@ function containsDuplicate(nums: number[]): boolean {
 test('containsDuplicate', () => {
   expect(containsDuplicate([1, 2, 3, 1])).toBe(true)
   expect(containsDuplicate([1, 2, 3, 4])).toBe(false)
+  expect(containsDuplicate([1])).toBe(false)
 })
