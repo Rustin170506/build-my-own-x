648b7c7772460b70dbcfb07681e306382d3cef9b
hi-rustin <rustin.liu@gmail.com>
2023-05-14 10:13:21 +0800

    tr: add eslint-config-prettier
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- tr/.eslintrc.cjs
+++ tr/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
