7ca4cfeb87b81e0b244868efbaa28aa86514f9a1
hi-rustin <rustin.liu@gmail.com>
2023-05-14 10:11:25 +0800

    tr: add prettier
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- tr/src/q-270-contains-duplicate.ts
+++ tr/src/q-270-contains-duplicate.ts
@@ -1,24 +1,24 @@
-import { expect, test } from 'vitest'
+import { expect, test } from "vitest";
 
 function containsDuplicate(nums: number[]): boolean {
-    if (nums.length == 1) {
-        return false;
-    }
-
-    const set = new Set<number>();
-    for (const num of nums) {
-        if (set.has(num)) {
-        return true;
-        } else {
-        set.add(num);
-        }
-    }
+  if (nums.length == 1) {
     return false;
+  }
+
+  const set = new Set<number>();
+  for (const num of nums) {
+    if (set.has(num)) {
+      return true;
+    } else {
+      set.add(num);
+    }
+  }
+  return false;
 }
 
-test('containsDuplicate', () => {
-  expect(containsDuplicate([1, 2, 3, 1])).toBe(true)
-  expect(containsDuplicate([1, 2, 3, 4])).toBe(false)
-  expect(containsDuplicate([1])).toBe(false)
-  expect(containsDuplicate([1, 1])).toBe(true)
-})
+test("containsDuplicate", () => {
+  expect(containsDuplicate([1, 2, 3, 1])).toBe(true);
+  expect(containsDuplicate([1, 2, 3, 4])).toBe(false);
+  expect(containsDuplicate([1])).toBe(false);
+  expect(containsDuplicate([1, 1])).toBe(true);
+});
