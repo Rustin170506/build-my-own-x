4630c0e712a7955359419ad272a00aee4fc4d01f
hi-rustin <rustin.liu@gmail.com>
2023-05-15 22:38:33 +0800

    lr: fix q_746
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_746_min_cost_climbing_stairs.rs
+++ lr/src/questions/q_746_min_cost_climbing_stairs.rs
@@ -4,7 +4,7 @@ pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
     let mut cost = cost;
     cost.push(0);
 
-    for i in cost.len() - 3..0 {
+    for i in (0..cost.len() - 3).rev() {
         cost[i] += min(cost[i + 1], cost[i + 2])
     }
 
@@ -17,6 +17,6 @@ mod tests {
 
     #[test]
     fn test_min_cost_climbing_stairs() {
-        assert_eq!(min_cost_climbing_stairs(vec![10, 5, 20]), 5);
+        assert_eq!(min_cost_climbing_stairs(vec![10, 15, 20]), 15);
     }
 }
