5cd09225be877256cdee5d84f3d764f7e71713e3
hi-rustin <rustin.liu@gmail.com>
2023-06-07 21:27:33 +0800

    lr: q_268
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_268_missing_number.rs
+++ lr/src/questions/q_268_missing_number.rs
@@ -1,5 +1,5 @@
 pub fn missing_number(nums: Vec<i32>) -> i32 {
-    let mut res = nums.len() as i32;
+    let mut res = (nums.len()) as i32;
 
     for (i, n) in nums.iter().enumerate() {
         res += (i as i32) - n;
