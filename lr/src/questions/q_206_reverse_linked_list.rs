35a8c480fc681640cec4ff5265b5866ee872ec79
hi-rustin <rustin.liu@gmail.com>
2023-06-11 11:09:47 +0800

    lr: improve q_206
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_206_reverse_linked_list.rs
+++ lr/src/questions/q_206_reverse_linked_list.rs
@@ -1,40 +1,47 @@
 use crate::utils::ListNode;
 
 pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let (mut pre, mut cuur) = (None, head);
-
-    while let Some(mut node) = cuur {
-        cuur = node.next.take();
-        node.next = pre;
-        pre = Some(node);
+    let (mut prev, mut curr) = (None, head);
+    while let Some(mut node) = curr {
+        curr = node.next.take();
+        node.next = prev;
+        prev = Some(node);
     }
 
-    pre
+    prev
 }
 
 pub fn reverse_list_recursive(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    fn reverse(pre: Option<Box<ListNode>>, cur: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        if let Some(mut node) = cur {
+    fn reverse(prev: Option<Box<ListNode>>, curr: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if let Some(mut node) = curr {
             let next = node.next.take();
-            node.next = pre;
+            node.next = prev;
             reverse(Some(node), next)
         } else {
-            pre
+            prev
         }
     }
 
     reverse(None, head)
 }
 
-#[cfg(test)]
-mod tests {
+#[test]
+fn test_reverse_list() {
     use crate::list;
 
-    use super::*;
+    assert_eq!(reverse_list(list!()), list!());
+    assert_eq!(reverse_list(list!(1, 2, 3, 4, 5)), list!(5, 4, 3, 2, 1));
+    assert_eq!(reverse_list(list!(1)), list!(1));
+}
 
-    #[test]
-    fn test_reverse_list() {
-        assert_eq!(reverse_list(list!(1, 2, 3)), list!(3, 2, 1));
-        assert_eq!(reverse_list_recursive(list!(1, 2, 3)), list!(3, 2, 1));
-    }
+#[test]
+fn test_reverse_list_recursive() {
+    use crate::list;
+
+    assert_eq!(reverse_list_recursive(list!()), list!());
+    assert_eq!(
+        reverse_list_recursive(list!(1, 2, 3, 4, 5)),
+        list!(5, 4, 3, 2, 1)
+    );
+    assert_eq!(reverse_list_recursive(list!(1)), list!(1));
 }
