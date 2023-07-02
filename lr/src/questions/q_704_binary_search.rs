a7830d84d61513917ae282cc7b53d7883356de08
hi-rustin <rustin.liu@gmail.com>
2023-07-02 23:59:19 +0800

    lr: rewrite q_704
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_704_binary_search.rs
+++ lr/src/questions/q_704_binary_search.rs
@@ -1,31 +1,27 @@
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-    let mut result = -1;
-    let mut left = 0_i32;
-    let mut right = (nums.len() - 1) as i32;
+    let mut res = -1;
+    let (mut left, mut right) = (0, nums.len() - 1);
     while left <= right {
-        let mid = (left + right) / 2;
-        match nums[mid as usize].cmp(&target) {
-            std::cmp::Ordering::Less => left = mid + 1,
+        let m = (left + right) / 2;
+        match nums[m].cmp(&target) {
+            std::cmp::Ordering::Less => {
+                left = m + 1;
+            }
             std::cmp::Ordering::Equal => {
-                result = mid;
+                res = m as i32;
                 break;
             }
-            std::cmp::Ordering::Greater => right = mid - 1,
+            std::cmp::Ordering::Greater => {
+                right = m - 1;
+            }
         }
     }
-    result
+
+    res
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_search() {
-        assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 9), 4);
-        assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 2), -1);
-        assert_eq!(search(vec![5], 5), 0);
-        assert_eq!(search(vec![5], -5), -1);
-        assert_eq!(search(vec![2, 5], 5), 1);
-    }
+#[test]
+fn test_search() {
+    assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 9), 4);
+    assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 2), -1);
 }
