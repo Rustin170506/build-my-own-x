25fbac34f7c802a54656845454035036604d1fc3
hi-rustin <rustin.liu@gmail.com>
2023-08-27 10:33:36 +0800

    lr: q_226

--- lr/src/questions/q_226_invert_binary_tree.rs
+++ lr/src/questions/q_226_invert_binary_tree.rs
@@ -1,16 +1,16 @@
-use std::{cell::RefCell, mem::replace, rc::Rc};
-
 use crate::utils::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
     if root.is_none() {
         return root;
     }
 
-    let tmp1 = root.as_ref().unwrap().borrow_mut().left.take();
-    let tmp2 = root.as_ref().unwrap().borrow_mut().right.take();
-    let _ = replace(&mut root.as_ref().unwrap().borrow_mut().right, tmp1);
-    let _ = replace(&mut root.as_ref().unwrap().borrow_mut().left, tmp2);
+    let temp1 = root.as_ref().unwrap().borrow_mut().left.take();
+    let temp2 = root.as_ref().unwrap().borrow_mut().right.take();
+    let _ = std::mem::replace(&mut root.as_ref().unwrap().borrow_mut().right, temp1);
+    let _ = std::mem::replace(&mut root.as_ref().unwrap().borrow_mut().left, temp2);
 
     invert_tree(root.as_ref().unwrap().borrow_mut().left.clone());
     invert_tree(root.as_ref().unwrap().borrow_mut().right.clone());
@@ -18,19 +18,13 @@ pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<Tre
     root
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn test_invert_tree() {
     use crate::tree;
 
-    #[test]
-    fn test_invert_tree() {
-        assert_eq!(invert_tree(tree!()), tree!());
-        assert_eq!(invert_tree(tree!(1)), tree!(1));
-        assert_eq!(invert_tree(tree!(1, 2, 3)), tree!(1, 3, 2));
-        assert_eq!(
-            invert_tree(tree!(4, 2, 7, 1, 3, 6, 9)),
-            tree!(4, 7, 2, 9, 6, 3, 1)
-        );
-    }
+    assert_eq!(
+        invert_tree(tree!(4, 2, 7, 1, 3, 6, 9)),
+        tree!(4, 7, 2, 9, 6, 3, 1)
+    );
+    assert_eq!(invert_tree(tree!()), tree!());
 }
