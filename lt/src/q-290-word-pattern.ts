ea9acd80feb8cf579b3b13a9f8f5e7286e0c3fc0
hi-rustin <rustin.liu@gmail.com>
2023-11-20 20:27:59 +0800

    lt: fix format
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lt/src/q-290-word-pattern.ts
+++ lt/src/q-290-word-pattern.ts
@@ -1,36 +1,36 @@
 import { test, expect } from "vitest";
 
-  function wordPattern(pattern: string, s: string): boolean {
-    const words = s.split(" ");
-    if (pattern.length !== words.length) {
-      return false;
-    }
-    const patternMap = new Map();
-    const wordsMap = new Map();
-
-    for (let index = 0; index < pattern.length; index++) {
-      const word = words[index];
-      const c = pattern.charAt(index);
-      if (patternMap.has(c)) {
-        const w = patternMap.get(c);
-        if (w !== word) {
-          return false;
-        }
-      } else {
-        patternMap.set(c, word);
-      }
-      if (wordsMap.has(word)) {
-        const char = wordsMap.get(word);
-        if (c !== char) {
-          return false;
-        }
-      } else {
-        wordsMap.set(word, c);
-      }
-    }
-
-    return true;
+function wordPattern(pattern: string, s: string): boolean {
+  const words = s.split(" ");
+  if (pattern.length !== words.length) {
+    return false;
   }
+  const patternMap = new Map();
+  const wordsMap = new Map();
+
+  for (let index = 0; index < pattern.length; index++) {
+    const word = words[index];
+    const c = pattern.charAt(index);
+    if (patternMap.has(c)) {
+      const w = patternMap.get(c);
+      if (w !== word) {
+        return false;
+      }
+    } else {
+      patternMap.set(c, word);
+    }
+    if (wordsMap.has(word)) {
+      const char = wordsMap.get(word);
+      if (c !== char) {
+        return false;
+      }
+    } else {
+      wordsMap.set(word, c);
+    }
+  }
+
+  return true;
+}
 
 test("wordPattern", () => {
   expect(wordPattern("abba", "dog cat cat dog")).toBe(true);
