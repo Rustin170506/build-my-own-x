36aa26e47bfa0e7b596ab32d0c84e92b837fff2c
hi-rustin <rustin.liu@gmail.com>
2023-11-21 20:17:54 +0800

    lt: table width 4
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lt/src/q0290WordPattern.ts
+++ lt/src/q0290WordPattern.ts
@@ -1,31 +1,31 @@
 export function wordPattern(pattern: string, s: string): boolean {
-  const words = s.split(" ");
-  if (pattern.length !== words.length) {
-    return false;
-  }
-  const patternMap = new Map();
-  const wordsMap = new Map();
-
-  for (let index = 0; index < pattern.length; index++) {
-    const word = words[index];
-    const c = pattern.charAt(index);
-    if (patternMap.has(c)) {
-      const w = patternMap.get(c);
-      if (w !== word) {
+    const words = s.split(" ");
+    if (pattern.length !== words.length) {
         return false;
-      }
-    } else {
-      patternMap.set(c, word);
     }
-    if (wordsMap.has(word)) {
-      const char = wordsMap.get(word);
-      if (c !== char) {
-        return false;
-      }
-    } else {
-      wordsMap.set(word, c);
-    }
-  }
+    const patternMap = new Map();
+    const wordsMap = new Map();
 
-  return true;
+    for (let index = 0; index < pattern.length; index++) {
+        const word = words[index];
+        const c = pattern.charAt(index);
+        if (patternMap.has(c)) {
+            const w = patternMap.get(c);
+            if (w !== word) {
+                return false;
+            }
+        } else {
+            patternMap.set(c, word);
+        }
+        if (wordsMap.has(word)) {
+            const char = wordsMap.get(word);
+            if (c !== char) {
+                return false;
+            }
+        } else {
+            wordsMap.set(word, c);
+        }
+    }
+
+    return true;
 }
