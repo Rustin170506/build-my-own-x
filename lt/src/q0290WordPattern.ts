c35d4686acd34b49b672b3c9acc60a4674a7f32f
hi-rustin <rustin.liu@gmail.com>
2023-11-23 22:01:44 +0800

    lr: q0290
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lt/src/q0290WordPattern.ts
+++ lt/src/q0290WordPattern.ts
@@ -3,27 +3,21 @@ export function wordPattern(pattern: string, s: string): boolean {
     if (pattern.length !== words.length) {
         return false;
     }
-    const patternMap = new Map();
-    const wordsMap = new Map();
+    const patternMap: Map<string, number> = new Map();
+    const wordsMap: Map<string, number> = new Map();
 
     for (let index = 0; index < pattern.length; index++) {
         const word = words[index];
-        const c = pattern.charAt(index);
-        if (patternMap.has(c)) {
-            const w = patternMap.get(c);
-            if (w !== word) {
-                return false;
-            }
-        } else {
-            patternMap.set(c, word);
+        const c = pattern[index];
+        if (!patternMap.has(c)) {
+            patternMap.set(c, index);
         }
-        if (wordsMap.has(word)) {
-            const char = wordsMap.get(word);
-            if (c !== char) {
-                return false;
-            }
-        } else {
-            wordsMap.set(word, c);
+        if (!wordsMap.has(word)) {
+            wordsMap.set(word, index);
+        }
+
+        if (patternMap.get(c) !== wordsMap.get(word)) {
+            return false;
         }
     }
 
