c102830ee4914f2e4f3fcf7195345a892e38c581
hi-rustin <rustin.liu@gmail.com>
2023-11-26 16:20:58 +0800

    lr: q_0540
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_0540_single_element_in_a_sorted_array.rs
+++ lr/src/questions/q_0540_single_element_in_a_sorted_array.rs
@@ -1,30 +1,29 @@
 pub fn single_non_duplicate(nums: Vec<i32>) -> i32 {
-    let (mut i, mut j) = (0, nums.len() - 1);
-    let (mut i_count, mut j_count) = (1, 1);
-    while i < j {
-        if j - 1 > 0 && nums[j - 1] == nums[j] {
-            j_count += 1;
-            j -= 1;
+    let (mut l, mut r) = (0, nums.len() - 1);
+    while l <= r {
+        let m = l + (r - l) / 2;
+
+        if (m as i32 - 1 < 0 || nums[m] != nums[m - 1])
+            && (m + 1 > nums.len() - 1 || nums[m] != nums[m + 1])
+        {
+            return nums[m];
         }
-        if i + 1 < (nums.len() - 1) && nums[i + 1] == nums[i] {
-            i_count += 1;
-            i += 1;
-        }
-        if j_count == 2 {
-            j_count = 1;
-            j -= 1;
-        }
-        if i_count == 2 {
-            i_count = 1;
-            i += 1;
+
+        let left_size = if nums[m - 1] == nums[m] { m - 1 } else { m };
+
+        if left_size % 2 == 0 {
+            l = m + 1;
+        } else {
+            r = m - 1;
         }
     }
 
-    nums[i]
+    unreachable!()
 }
 
 #[test]
 fn test_single_non_duplicate() {
     assert_eq!(single_non_duplicate(vec![1, 1, 2, 3, 3, 4, 4, 8, 8]), 2);
     assert_eq!(single_non_duplicate(vec![3, 3, 7, 7, 10, 11, 11]), 10);
+    assert_eq!(single_non_duplicate(vec![1, 1, 2, 2, 3]), 3);
 }
