97b3086a7fcdd911a8a7e9ec1d49b81e2774cbc6
hi-rustin <rustin.liu@gmail.com>
2023-11-27 09:03:49 +0800

    lr: q_0143 better code
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_0143_reorder_list.rs
+++ lr/src/questions/q_0143_reorder_list.rs
@@ -1,51 +1,53 @@
 use crate::utils::ListNode;
 
 pub fn reorder_list(head: &mut Option<Box<ListNode>>) {
-    let mut left = head.take();
-    let mut n = 0;
-    let mut p = &left;
-    while let Some(node) = p {
-        p = &node.next;
-        n += 1;
+    let mut curr = head.take();
+    let mut length = 0;
+    let mut temp = &curr;
+
+    // Calculate the length of the list
+    while let Some(node) = temp {
+        temp = &node.next;
+        length += 1;
     }
-    // Only one node, do nothing.
-    if n < 2 {
-        *head = left.take();
+
+    if length < 2 {
+        *head = curr.take();
         return;
     }
 
-    // Find the middle node.
-    let mut p = &mut left;
-    for _ in 0..(n + 1) / 2 {
-        p = &mut p.as_mut().unwrap().next;
+    // Split the list into two halves
+    let mut first_half = &mut curr;
+    for _ in 0..(length + 1) / 2 {
+        first_half = &mut first_half.as_mut().unwrap().next;
     }
 
-    // Reverse the right half.
-    let mut right = None;
-    let mut p = p.take();
-    while let Some(mut node) = p {
-        p = node.next.take();
-        node.next = right;
-        right = Some(node);
+    // Reverse the second half of the list
+    let mut second_half = None;
+    let mut temp = first_half.take();
+    while let Some(mut node) = temp {
+        temp = node.next.take();
+        node.next = second_half;
+        second_half = Some(node);
     }
 
-    let mut new_head = None;
-    let mut next = &mut new_head;
+    // Merge the two halves
+    let mut merged_head = None;
+    let mut merged_tail = &mut merged_head;
 
-    while let Some(mut node) = left {
-        // Move the left node to the new list.
-        left = node.next.take();
-        *next = Some(node);
-        next = &mut next.as_mut().unwrap().next;
-        // Move the right node to the new list.
-        if let Some(mut node) = right {
-            right = node.next.take();
-            *next = Some(node);
-            next = &mut next.as_mut().unwrap().next;
+    while let Some(mut left_node) = curr {
+        curr = left_node.next.take();
+        *merged_tail = Some(left_node);
+        merged_tail = &mut merged_tail.as_mut().unwrap().next;
+
+        if let Some(mut right_node) = second_half {
+            second_half = right_node.next.take();
+            *merged_tail = Some(right_node);
+            merged_tail = &mut merged_tail.as_mut().unwrap().next;
         }
     }
-    // Handle the dummy node.
-    *head = new_head.take();
+
+    *head = merged_head.take();
 }
 
 #[cfg(test)]
@@ -57,6 +59,6 @@ mod tests {
     fn test_reorder_list() {
         let mut list = list!(1, 2, 3, 4);
         reorder_list(&mut list);
-        assert_eq!(list, list!(1, 4, 2, 3))
+        assert_eq!(list, list!(1, 4, 2, 3));
     }
 }
