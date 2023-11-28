1816f3cd82f1f3619aa88a841240bb4886000e66
hi-rustin <rustin.liu@gmail.com>
2023-11-28 09:08:35 +0800

    lt: q0143
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

---
+++ lt/src/q0143ReorderList.test.ts
@@ -0,0 +1,36 @@
+import { reorderList, ListNode } from "./q0143ReorderList";
+
+function createLinkedList(arr: number[]): ListNode | null {
+    if (arr.length === 0) return null;
+    const head: ListNode = new ListNode(arr[0]);
+    let current = head;
+    for (let i = 1; i < arr.length; i++) {
+        current.next = new ListNode(arr[i]);
+        current = current.next;
+    }
+    return head;
+}
+
+function convertLinkedListToList(head: ListNode | null): number[] {
+    const arr: number[] = [];
+    let current = head;
+    while (current !== null) {
+        arr.push(current.val);
+        current = current.next;
+    }
+    return arr;
+}
+
+test("reorderList", () => {
+    let list = createLinkedList([1, 2, 3, 4]);
+    reorderList(list);
+    expect(convertLinkedListToList(list)).toEqual([1, 4, 2, 3]);
+
+    list = createLinkedList([1, 2, 3, 4, 5]);
+    reorderList(list);
+    expect(convertLinkedListToList(list)).toEqual([1, 5, 2, 4, 3]);
+
+    list = createLinkedList([]);
+    reorderList(list);
+    expect(convertLinkedListToList(list)).toEqual([]);
+});
---
+++ lt/src/q0143ReorderList.ts
@@ -0,0 +1,46 @@
+export class ListNode {
+    constructor(
+        public val = 0,
+        public next: ListNode | null = null,
+    ) {}
+}
+
+export function reorderList(head: ListNode | null): void {
+    if (!head) return;
+
+    // Find the middle of the list.
+    let slow = head,
+        fast = head;
+    while (fast.next && fast.next.next) {
+        slow = slow.next!;
+        fast = fast.next.next;
+    }
+
+    // Split the list into two halves.
+    let secondHalf = slow.next;
+    slow.next = null;
+
+    // Reverse the second half.
+    let prev = null,
+        current = secondHalf;
+    while (current) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    secondHalf = prev;
+
+    // Merge the two halves.
+    let firstHalf: ListNode | null = head;
+    while (firstHalf && secondHalf) {
+        const nextFirstHalf: ListNode | null = firstHalf.next;
+        const nextSecondHalf = secondHalf.next;
+
+        firstHalf.next = secondHalf;
+        secondHalf.next = nextFirstHalf;
+
+        firstHalf = nextFirstHalf;
+        secondHalf = nextSecondHalf;
+    }
+}
