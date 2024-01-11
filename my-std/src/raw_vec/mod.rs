2e297964fd2d2186627762c99d389cd74d0d88de
hi-rustin <rustin.liu@gmail.com>
2024-01-11 09:39:37 +0800

    fix: use inline
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- my-std/src/raw_vec/mod.rs
+++ my-std/src/raw_vec/mod.rs
@@ -31,11 +31,13 @@ impl<T> RawVec<T, Global> {
     }
 
     #[must_use]
+    #[inline]
     pub fn with_capacity(cap: usize) -> Self {
         Self::with_capacity_in(cap, Global)
     }
 
     #[must_use]
+    #[inline]
     pub fn with_capacity_zeroed(capacity: usize) -> Self {
         Self::with_capacity_zeroed_in(capacity, Global)
     }
@@ -56,10 +58,12 @@ impl<T, A: Allocator> RawVec<T, A> {
         }
     }
 
+    #[inline]
     pub fn with_capacity_in(capacity: usize, alloc: A) -> Self {
         Self::allocate_in(capacity, AllocInit::Uninitialized, alloc)
     }
 
+    #[inline]
     pub fn with_capacity_zeroed_in(capacity: usize, alloc: A) -> Self {
         Self::allocate_in(capacity, AllocInit::Zeroed, alloc)
     }
@@ -68,6 +72,8 @@ impl<T, A: Allocator> RawVec<T, A> {
         if T::IS_ZST || capacity == 0 {
             Self::new_in(alloc)
         } else {
+            // We avoid `unwrap_or_else` here because it bloats the amount of
+            // LLVM IR generated.
             let layout = match Layout::array::<T>(capacity) {
                 Ok(layout) => layout,
                 Err(_) => capacity_overflow(),
@@ -84,7 +90,9 @@ impl<T, A: Allocator> RawVec<T, A> {
                 Ok(ptr) => ptr,
                 Err(_) => handle_alloc_error(layout),
             };
-
+            // Allocators currently return a `NonNull<[u8]>` whose length
+            // matches the size requested. If that ever changes, the capacity
+            // here should change to `ptr.len() / mem::size_of::<T>()`.
             Self {
                 ptr: unsafe { NonNull::new_unchecked(ptr.cast().as_ptr()) },
                 cap: Cap(capacity),
@@ -93,6 +101,17 @@ impl<T, A: Allocator> RawVec<T, A> {
         }
     }
 
+    /// Gets a raw pointer to the start of the allocation.
+    /// Note that this is `NonNull::dangling()` if `capacity == 0` or `T` is a ZST.
+    /// #[inline]
+    pub fn ptr(&self) -> *mut T {
+        self.ptr.as_ptr()
+    }
+
+    /// Gets the capacity of the allocation.
+    ///
+    /// This will always return `usize::MAX` if `T` is a ZST.
+    #[inline(always)]
     pub fn capacity(&self) -> usize {
         if T::IS_ZST {
             usize::MAX
@@ -101,15 +120,16 @@ impl<T, A: Allocator> RawVec<T, A> {
         }
     }
 
-    pub fn ptr(&self) -> *mut T {
-        self.ptr.as_ptr()
-    }
-
+    /// Returns a shared reference to the allocator backing this `RawVec`.
     pub fn allocator(&self) -> &A {
         &self.alloc
     }
 }
 
+// We need to guarantee the following:
+// * We don't ever allocate `> isize::MAX` byte-size objects.
+// * We don't overflow `usize::MAX` and actually allocate too little.
+#[inline]
 fn alloc_guard(alloc_size: usize) -> Result<(), TryReserveError> {
     if usize::BITS < 64 && alloc_size > isize::MAX as usize {
         Err(TryReserveErrorKind::CapacityOverflow.into())
@@ -118,6 +138,9 @@ fn alloc_guard(alloc_size: usize) -> Result<(), TryReserveError> {
     }
 }
 
+// One central function responsible for reporting capacity overflows. This'll
+// ensure that the code generation related to these panics is minimal as there's
+// only one location which panics rather than a bunch throughout the module.
 fn capacity_overflow() -> ! {
     panic!("capacity overflow");
 }
