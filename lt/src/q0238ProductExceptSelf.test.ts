d02c533d90adc8324e25f3a3a4de8ecbe6f22899
hi-rustin <rustin.liu@gmail.com>
2024-01-18 11:52:29 +0800

    lt: q0238
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lt/src/q0238ProductExceptSelf.test.ts
+++ lt/src/q0238ProductExceptSelf.test.ts
@@ -1,5 +1,8 @@
-import { productExceptSelf } from "./q0238ProductExceptSelf";
+import {
+    productExceptSelf,
+    productExceptSelf1,
+} from "./q0238ProductExceptSelf";
 test("productExceptSelf", () => {
-    expect(productExceptSelf([1, 2, 3, 4])).toStrictEqual([24, 12, 8, 6]);
+    expect(productExceptSelf1([1, 2, 3, 4])).toStrictEqual([24, 12, 8, 6]);
     expect(productExceptSelf([-1, 1, 0, -3, 3])).toStrictEqual([0, 0, 9, 0, 0]);
 });
--- lt/src/q0238ProductExceptSelf.ts
+++ lt/src/q0238ProductExceptSelf.ts
@@ -17,3 +17,20 @@ export function productExceptSelf(nums: number[]): number[] {
     }
     return result;
 }
+
+export function productExceptSelf1(nums: number[]): number[] {
+    const result = new Array(nums.length).fill(1);
+    let prefix = 1;
+    for (let index = 0; index < nums.length; index++) {
+        result[index] = prefix;
+        prefix *= nums[index];
+    }
+
+    let postfix = 1;
+    for (let index = nums.length - 1; index >= 0; index--) {
+        result[index] *= postfix;
+        postfix *= nums[index];
+    }
+
+    return result;
+}
