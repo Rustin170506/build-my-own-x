1ebbe1dce68bf94c2a63a9fef453215d110c5f32
hi-rustin <rustin.liu@gmail.com>
2024-01-21 16:35:01 +0800

    lt: q0167
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

---
+++ lt/src/q0167TwoII.test.ts
@@ -0,0 +1,7 @@
+import { twoSum } from "./q0167TwoII";
+
+describe("twoSum ", () => {
+    it("should be return [1, 2]", () => {
+        expect(twoSum([2, 7, 11, 15], 9)).toEqual([1, 2]);
+    });
+});
