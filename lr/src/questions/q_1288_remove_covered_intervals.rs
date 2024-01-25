3d795b7b0c47520db8241da6da89c60970f12cee
hi-rustin <rustin.liu@gmail.com>
2024-01-25 09:33:33 +0800

    lr: q_1288
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_1288_remove_covered_intervals.rs
+++ lr/src/questions/q_1288_remove_covered_intervals.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 pub fn remove_covered_intervals(intervals: Vec<Vec<i32>>) -> i32 {
     let mut result = 0;
     for i in 0..intervals.len() {
@@ -20,11 +22,42 @@ pub fn remove_covered_intervals(intervals: Vec<Vec<i32>>) -> i32 {
     return result;
 }
 
+pub fn remove_covered_intervals_v2(intervals: Vec<Vec<i32>>) -> i32 {
+    let mut intervals = intervals;
+    intervals.sort_by(|a, b| {
+        if a[0] < b[0] {
+            Ordering::Less
+        } else if a[0] > b[0] {
+            Ordering::Greater
+        } else if a[1] < b[1] {
+            Ordering::Greater
+        } else {
+            Ordering::Less
+        }
+    });
+
+    let mut count = 0;
+    let mut end = std::i32::MIN;
+    for interval in intervals {
+        if interval[1] > end {
+            end = interval[1];
+            count += 1;
+        }
+    }
+
+    count
+}
+
 #[test]
 fn test_remove_covered_intervals() {
     assert_eq!(
         remove_covered_intervals(vec![vec![1, 4], vec![3, 6], vec![2, 8]]),
         2
     );
-    assert_eq!(remove_covered_intervals(vec![vec![1, 4], vec![2, 3]]), 1)
+    assert_eq!(remove_covered_intervals(vec![vec![1, 4], vec![2, 3]]), 1);
+    assert_eq!(
+        remove_covered_intervals_v2(vec![vec![1, 4], vec![3, 6], vec![2, 8]]),
+        2
+    );
+    assert_eq!(remove_covered_intervals_v2(vec![vec![1, 4], vec![2, 3]]), 1);
 }
