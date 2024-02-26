2a5f6bcb312deb7a7c1b6828a8a9eee14fccffcf
hi-rustin <rustin.liu@gmail.com>
2024-02-26 09:03:19 +0800

    lg: q_0003
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

---
+++ lg/src/questions/q_0003_longest_substring_without_repeating_characters_test.go
@@ -0,0 +1,11 @@
+package questions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	assert.Equal(t, 3, lengthOfLongestSubstring("abcabcbb"))
+}
