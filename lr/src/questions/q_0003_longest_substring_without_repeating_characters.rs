fcd9d67a1e3d02d8ed766b103f3a190fa3e438a2
hi-rustin <rustin.liu@gmail.com>
2024-02-29 08:54:42 +0800

    lr: q_0003
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_0003_longest_substring_without_repeating_characters.rs
+++ lr/src/questions/q_0003_longest_substring_without_repeating_characters.rs
@@ -22,14 +22,38 @@ pub fn length_of_longest_substring(s: String) -> i32 {
     result
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_length_of_longest_substring() {
-        assert_eq!(length_of_longest_substring("".to_string()), 0);
-        assert_eq!(length_of_longest_substring("abcabcbb".to_string()), 3);
-        assert_eq!(length_of_longest_substring("aaaaaa".to_string()), 1);
+pub fn length_of_longest_substring_brute_force(s: String) -> i32 {
+    if s.is_empty() {
+        return 0;
     }
+    let s: Vec<char> = s.chars().collect();
+
+    let mut result = 0;
+    for i in 0..s.len() {
+        let mut seen: Vec<bool> = vec![false; 128];
+        let mut j = i;
+        while j < s.len() && !seen[s[j] as usize] {
+            seen[s[j] as usize] = true;
+            j += 1;
+        }
+        result = result.max(j - i);
+    }
+
+    result as i32
+}
+
+#[test]
+fn test_length_of_longest_substring() {
+    assert_eq!(length_of_longest_substring("".to_string()), 0);
+    assert_eq!(length_of_longest_substring("abcabcbb".to_string()), 3);
+    assert_eq!(length_of_longest_substring("aaaaaa".to_string()), 1);
+    assert_eq!(length_of_longest_substring_brute_force("".to_string()), 0);
+    assert_eq!(
+        length_of_longest_substring_brute_force("abcabcbb".to_string()),
+        3
+    );
+    assert_eq!(
+        length_of_longest_substring_brute_force("aaaaaa".to_string()),
+        1
+    );
 }
