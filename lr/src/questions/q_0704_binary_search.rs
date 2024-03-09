c0ea750a0d86c04574c3997e0a5a61ebffe35ecb
hi-rustin <rustin.liu@gmail.com>
2024-03-09 17:27:17 +0800

    lr: q_0704
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_0704_binary_search.rs
+++ lr/src/questions/q_0704_binary_search.rs
@@ -20,8 +20,26 @@ pub fn search(nums: Vec<i32>, target: i32) -> i32 {
     res
 }
 
+pub fn search_recursive(nums: Vec<i32>, target: i32) -> i32 {
+    fn helper(nums: &[i32], target: i32, left: usize, right: usize) -> i32 {
+        if left > right {
+            return -1;
+        }
+        let m = left + (right - left) / 2;
+        match nums[m].cmp(&target) {
+            std::cmp::Ordering::Less => helper(nums, target, m + 1, right),
+            std::cmp::Ordering::Equal => m as i32,
+            std::cmp::Ordering::Greater => helper(nums, target, left, m - 1),
+        }
+    }
+
+    helper(&nums, target, 0, nums.len() - 1)
+}
+
 #[test]
 fn test_search() {
     assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 9), 4);
     assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 2), -1);
+    assert_eq!(search_recursive(vec![-1, 0, 3, 5, 9, 12], 9), 4);
+    assert_eq!(search_recursive(vec![-1, 0, 3, 5, 9, 12], 2), -1);
 }
