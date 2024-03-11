0b4fbff5f5cfbdb3589b188eb1f02fb567347ac9
hi-rustin <rustin.liu@gmail.com>
2024-03-11 08:22:33 +0800

    lg: q0143 fast and slow pointers
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lg/src/questions/q0143_reorder_list.go
+++ lg/src/questions/q0143_reorder_list.go
@@ -46,3 +46,38 @@ func reorderList(head *utils.ListNode) {
 		second = tmp
 	}
 }
+
+func reorderListFastAndSlowPointers(head *utils.ListNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
+
+	// Find the middle of the list
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	// Reverse the second half of the list
+	var prev *utils.ListNode
+	current := slow
+	for current != nil {
+		next := current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+
+	// Merge the two halves
+	first, second := head, prev
+	for second.Next != nil {
+		tmp := first.Next
+		first.Next = second
+		first = tmp
+
+		tmp = second.Next
+		second.Next = first
+		second = tmp
+	}
+}
