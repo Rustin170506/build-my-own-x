b90baf51753038b1eece60d5da778866aa3f39df
hi-rustin <rustin.liu@gmail.com>
2024-03-14 08:44:39 +0800

    lr: q_0104
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_0104_maximum_depth_of_binary_tree.rs
+++ lr/src/questions/q_0104_maximum_depth_of_binary_tree.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 
 use crate::utils::TreeNode;
 
@@ -13,9 +13,36 @@ pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     i32::max(left, right)
 }
 
+pub fn max_depth_queue(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    if root.is_none() {
+        return 0;
+    }
+
+    let mut level = 0;
+    let mut queue: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+    queue.push_back(root);
+
+    while !queue.is_empty() {
+        let mut childen = VecDeque::new();
+        for n in queue {
+            if n.as_ref().unwrap().borrow().left.is_some() {
+                childen.push_back(n.as_ref().unwrap().borrow().left.to_owned());
+            }
+            if n.as_ref().unwrap().borrow().right.is_some() {
+                childen.push_back(n.as_ref().unwrap().borrow().right.to_owned());
+            }
+        }
+        level += 1;
+        queue = childen
+    }
+
+    level
+}
+
 #[test]
 fn test_max_depth() {
     use crate::tree;
 
-    assert_eq!(3, max_depth(tree!(3, 9, 20, None, None, 15, 7)))
+    assert_eq!(3, max_depth(tree!(3, 9, 20, None, None, 15, 7)));
+    assert_eq!(3, max_depth_queue(tree!(3, 9, 20, None, None, 15, 7)));
 }
