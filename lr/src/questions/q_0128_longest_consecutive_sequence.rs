1479cd2e70bff652c3d52ed500918e2b5db1785f
hi-rustin <rustin.liu@gmail.com>
2024-05-16 11:49:28 +0800

    lr: q_0128
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lr/src/questions/q_0128_longest_consecutive_sequence.rs
+++ lr/src/questions/q_0128_longest_consecutive_sequence.rs
@@ -6,12 +6,12 @@ pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
     }
 
     let mut result = 0;
-    let nums_set: HashSet<i32> = HashSet::from_iter(nums.clone());
+    let nums_set: HashSet<&i32> = HashSet::from_iter(nums.iter());
 
-    for n in nums {
+    for n in &nums {
         if nums_set.get(&(n - 1)).is_none() {
             let mut temp_count = 1;
-            let mut temp = n;
+            let mut temp = *n;
             while nums_set.get(&(temp + 1)).is_some() {
                 temp_count += 1;
                 temp += 1;
@@ -25,14 +25,9 @@ pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
     result
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_longest_consecutive() {
-        assert_eq!(longest_consecutive(vec![]), 0);
-        assert_eq!(longest_consecutive(vec![100, 4, 200, 1, 3, 2]), 4);
-        assert_eq!(longest_consecutive(vec![0, 3, 7, 2, 5, 8, 4, 6, 0, 1]), 9);
-    }
+#[test]
+fn test_longest_consecutive() {
+    assert_eq!(longest_consecutive(vec![]), 0);
+    assert_eq!(longest_consecutive(vec![100, 4, 200, 1, 3, 2]), 4);
+    assert_eq!(longest_consecutive(vec![0, 3, 7, 2, 5, 8, 4, 6, 0, 1]), 9);
 }
