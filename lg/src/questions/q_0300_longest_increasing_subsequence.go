d01951d09e381bb8a94004dde392aef1c4aa8ab9
hi-rustin <rustin.liu@gmail.com>
2024-06-06 22:57:37 +0800

    lg: q_0300_longest_increasing_subsequence with mem
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- lg/src/questions/q_0300_longest_increasing_subsequence.go
+++ lg/src/questions/q_0300_longest_increasing_subsequence.go
@@ -20,18 +20,24 @@ func lengthOfLIS(nums []int) int {
 
 func lengthOfLISRecursive(nums []int) int {
 	result := 0
+	mem := make(map[int]int, len(nums))
 	var dfs func(i int) int
 	dfs = func(i int) int {
 		if i >= len(nums) {
 			return 0
 		}
+		if _, ok := mem[i]; ok {
+			return mem[i]
+		}
 
 		temp := 1
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i] < nums[j] {
+
 				temp = max(temp, 1+dfs(j))
 			}
 		}
+		mem[i] = temp
 
 		return temp
 	}
