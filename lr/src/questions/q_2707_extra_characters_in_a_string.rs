b28a595b03738233f6e4936d126c848438ace856
hi-rustin <rustin.liu@gmail.com>
2024-07-02 10:09:44 +0800

    lr: q_2707 use trie

--- lr/src/questions/q_2707_extra_characters_in_a_string.rs
+++ lr/src/questions/q_2707_extra_characters_in_a_string.rs
@@ -1,12 +1,75 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
-pub fn min_extra_char(s: String, dictionary: Vec<String>) -> i32 {
-    let words = HashSet::from_iter(dictionary);
-    let mut dp = HashMap::new();
-    dfs(&s, 0, &words, &mut dp)
+struct TrieNode {
+    children: HashMap<char, TrieNode>,
+    end_of_word: bool,
 }
 
-fn dfs(s: &String, i: usize, words: &HashSet<String>, dp: &mut HashMap<usize, i32>) -> i32 {
+impl TrieNode {
+    fn new() -> Self {
+        TrieNode {
+            children: HashMap::new(),
+            end_of_word: false,
+        }
+    }
+}
+
+struct Trie {
+    root: TrieNode,
+}
+
+impl Trie {
+    fn new() -> Self {
+        Trie {
+            root: TrieNode::new(),
+        }
+    }
+
+    fn insert(&mut self, word: String) {
+        let mut cur = &mut self.root;
+        for c in word.chars() {
+            cur.children.entry(c).or_insert_with(TrieNode::new);
+            cur = cur.children.get_mut(&c).unwrap();
+        }
+
+        cur.end_of_word = true;
+    }
+
+    fn search(&self, word: String) -> bool {
+        let mut cur = &self.root;
+        for c in word.chars() {
+            if !cur.children.contains_key(&c) {
+                return false;
+            }
+            cur = cur.children.get(&c).unwrap();
+        }
+
+        cur.end_of_word
+    }
+
+    fn starts_with(&self, prefix: String) -> bool {
+        let mut cur = &self.root;
+        for c in prefix.chars() {
+            if !cur.children.contains_key(&c) {
+                return false;
+            }
+            cur = cur.children.get(&c).unwrap();
+        }
+
+        true
+    }
+}
+
+pub fn min_extra_char(s: String, dictionary: Vec<String>) -> i32 {
+    let mut trie = Trie::new();
+    for w in dictionary {
+        trie.insert(w);
+    }
+    let mut dp = HashMap::new();
+    dfs(&s, 0, &trie, &mut dp)
+}
+
+fn dfs(s: &String, i: usize, trie: &Trie, dp: &mut HashMap<usize, i32>) -> i32 {
     if i == s.len() {
         return 0;
     }
@@ -15,11 +78,16 @@ fn dfs(s: &String, i: usize, words: &HashSet<String>, dp: &mut HashMap<usize, i3
         return result;
     }
 
-    let mut result = 1 + dfs(s, i + 1, words, dp);
-
+    let mut result = 1 + dfs(s, i + 1, trie, dp);
+    let mut current = &trie.root;
     for j in i..s.len() {
-        if words.get(&s[i..j + 1]).is_some() {
-            result = i32::min(result, dfs(s, j + 1, words, dp));
+        let c = &s.chars().nth(j).unwrap();
+        if !current.children.contains_key(c) {
+            break;
+        }
+        current = current.children.get(c).unwrap();
+        if current.end_of_word {
+            result = i32::min(result, dfs(s, j + 1, trie, dp));
         }
     }
 
