9fd92e619e699045a83829f5941570379e5b5a8e
hi-rustin <rustin.liu@gmail.com>
2024-07-06 12:04:03 +0800

    lr: q_0105

--- lr/src/questions/q_0105_construct_binary_tree_from_preorder_and_inorder_traversal.rs
+++ lr/src/questions/q_0105_construct_binary_tree_from_preorder_and_inorder_traversal.rs
@@ -8,31 +8,23 @@ pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<Tr
             return None;
         }
 
-        let first_value = *preorder.first().unwrap();
-        let root = Some(Rc::new(RefCell::new(TreeNode::new(first_value))));
-        let mid = inorder.iter().position(|&i| i == first_value).unwrap();
+        let mut root = Some(Rc::new(RefCell::new(TreeNode::new(preorder[0]))));
+        let mid = inorder.iter().position(|&n| n == preorder[0]).unwrap();
 
-        root.as_ref().unwrap().borrow_mut().left = helper(&preorder[1..=mid], &inorder[..mid]);
-        root.as_ref().unwrap().borrow_mut().right =
+        root.as_mut().unwrap().borrow_mut().left = helper(&preorder[1..=mid], &inorder[..mid]);
+        root.as_mut().unwrap().borrow_mut().right =
             helper(&preorder[mid + 1..], &inorder[mid + 1..]);
-
         root
     }
-
     helper(&preorder, &inorder)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn test_build_tree() {
     use crate::tree;
-
-    #[test]
-    fn test_build_tree() {
-        assert_eq!(
-            build_tree(vec![3, 9, 20, 15, 7], vec![9, 3, 15, 20, 7]),
-            tree!(3, 9, 20, None, None, 15, 7)
-        );
-        assert_eq!(build_tree(vec![1], vec![1]), tree!(1));
-    }
+    assert_eq!(
+        build_tree(vec![3, 9, 20, 15, 7], vec![9, 3, 15, 20, 7]),
+        tree!(3, 9, 20, None, None, 15, 7)
+    );
+    assert_eq!(build_tree(vec![1], vec![1]), tree!(1));
 }
