b74c0be30e0792d1e09c82636ec091074b27220d
hi-rustin <rustin.liu@gmail.com>
2024-07-08 09:29:29 +0800

    lg: q_0098_validate_binary_search_tree.go better name

--- lg/src/questions/q_0098_validate_binary_search_tree.go
+++ lg/src/questions/q_0098_validate_binary_search_tree.go
@@ -8,17 +8,17 @@ import (
 
 func isValidBST(root *utils.TreeNode) bool {
 	var helper func(root *utils.TreeNode, minVal int, maxVal int) bool
-	helper = func(root *utils.TreeNode, minVal int, maxVal int) bool {
+	helper = func(root *utils.TreeNode, left int, right int) bool {
 		if root == nil {
 			return true
 		}
 
 		rootVal := root.Val
-		if rootVal <= minVal || rootVal >= maxVal {
+		if rootVal <= left || rootVal >= right {
 			return false
 		}
 
-		return helper(root.Left, minVal, rootVal) && helper(root.Right, rootVal, maxVal)
+		return helper(root.Left, left, rootVal) && helper(root.Right, rootVal, right)
 	}
 	return helper(root, math.MinInt, math.MaxInt)
 }
