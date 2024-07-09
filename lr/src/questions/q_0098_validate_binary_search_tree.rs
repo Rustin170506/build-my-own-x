e99c514208573ec96d3a34a5ded9001abea88119
hi-rustin <rustin.liu@gmail.com>
2024-07-09 09:36:07 +0800

    lr: q_0098_validate_binary_search_tree.rs

--- lr/src/questions/q_0098_validate_binary_search_tree.rs
+++ lr/src/questions/q_0098_validate_binary_search_tree.rs
@@ -27,15 +27,10 @@ pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
     valid(root, i64::MIN, i64::MAX)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn test_inorder_is_valid_bst() {
     use crate::tree;
-
-    #[test]
-    fn test_inorder_is_valid_bst() {
-        assert!(is_valid_bst(tree!(2147483647)));
-        assert!(is_valid_bst(tree!(2, 1, 3)));
-        assert!(!is_valid_bst(tree!(5, 1, 4, None, None, 3, 6)));
-    }
+    assert!(is_valid_bst(tree!(2147483647)));
+    assert!(is_valid_bst(tree!(2, 1, 3)));
+    assert!(!is_valid_bst(tree!(5, 1, 4, None, None, 3, 6)));
 }
