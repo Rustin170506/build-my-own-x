a66807af30fd314c6765f7ee51b198d5ced88d24
hi-rustin <rustin.liu@gmail.com>
2024-07-28 12:34:18 +0800

    lr: q_0994_rotting_oranges.rs

--- lr/src/questions/q_0994_rotting_oranges.rs
+++ lr/src/questions/q_0994_rotting_oranges.rs
@@ -2,66 +2,61 @@ use std::collections::VecDeque;
 
 pub fn oranges_rotting(grid: Vec<Vec<i32>>) -> i32 {
     let mut grid = grid;
-    let mut q = VecDeque::new();
-    let (mut time, mut fresh) = (0, 0);
+    let mut time = 0;
+    let mut queue = VecDeque::new();
+    let mut fresh = 0;
     let (rows, cols) = (grid.len(), grid[0].len());
-
-    for (r, row) in grid.iter().enumerate() {
-        for (c, e) in row.iter().enumerate() {
-            if *e == 1 {
-                fresh += 1;
-            }
-
-            if *e == 2 {
-                q.push_back((r, c));
+    let directions = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
+    for i in 0..rows {
+        for j in 0..cols {
+            match grid[i][j] {
+                1 => fresh += 1,
+                2 => queue.push_back((i, j)),
+                _ => {}
             }
         }
     }
 
-    let directions: Vec<(isize, isize)> = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
-    while !q.is_empty() && fresh > 0 {
-        let q_len = q.len();
+    while !queue.is_empty() && fresh != 0 {
+        let q_len = queue.len();
         for _ in 0..q_len {
-            let (r, c) = q.pop_front().unwrap();
-            for (dr, dc) in &directions {
-                let (row, col) = (dr + r as isize, dc + c as isize);
-
-                if (row < 0 || row == rows as isize)
-                    || (col < 0 || col == cols as isize)
-                    || grid[row as usize][col as usize] != 1
-                {
+            let (i, j) = queue.pop_front().unwrap();
+            for (x, y) in &directions {
+                let i = i as isize + x;
+                let j = j as isize + y;
+                if i < 0 || i >= rows as isize || j < 0 || j >= cols as isize {
                     continue;
                 }
-
-                grid[row as usize][col as usize] = 2;
-                q.push_back((row as usize, col as usize));
-                fresh -= 1;
+                let i = i as usize;
+                let j = j as usize;
+                match grid[i][j] {
+                    1 => {
+                        grid[i][j] = 2;
+                        fresh -= 1;
+                        queue.push_back((i, j));
+                    }
+                    _ => {}
+                }
             }
         }
-        time += 1
+        time += 1;
+    }
+    if fresh != 0 {
+        return -1;
     }
 
-    if fresh == 0 {
-        time
-    } else {
-        -1
-    }
+    time
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_oranges_rotting() {
-        assert_eq!(
-            oranges_rotting(vec![vec![2, 1, 1], vec![1, 1, 0], vec![0, 1, 1],]),
-            4
-        );
-        assert_eq!(
-            oranges_rotting(vec![vec![2, 1, 1], vec![0, 1, 1], vec![1, 0, 1],]),
-            -1
-        );
-        assert_eq!(oranges_rotting(vec![vec![0, 2],]), 0);
-    }
+#[test]
+fn test_oranges_rotting() {
+    assert_eq!(
+        oranges_rotting(vec![vec![2, 1, 1], vec![1, 1, 0], vec![0, 1, 1],]),
+        4
+    );
+    assert_eq!(
+        oranges_rotting(vec![vec![2, 1, 1], vec![0, 1, 1], vec![1, 0, 1],]),
+        -1
+    );
+    assert_eq!(oranges_rotting(vec![vec![0, 2],]), 0);
 }
