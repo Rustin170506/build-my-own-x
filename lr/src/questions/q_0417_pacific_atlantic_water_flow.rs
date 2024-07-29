b45345025261ffdc9c80e791b3a7d08b4bdae5ac
hi-rustin <rustin.liu@gmail.com>
2024-07-29 10:21:23 +0800

    lr: q_0417_pacific_atlantic_water_flow.rs

--- lr/src/questions/q_0417_pacific_atlantic_water_flow.rs
+++ lr/src/questions/q_0417_pacific_atlantic_water_flow.rs
@@ -1,55 +1,88 @@
 use std::collections::HashSet;
 
 pub fn pacific_atlantic(heights: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    if heights.is_empty() {
+        return vec![];
+    }
     let (rows, cols) = (heights.len(), heights[0].len());
-    let (mut pac, mut atl) = (HashSet::new(), HashSet::new());
     let mut result = vec![];
+    let mut pacific = HashSet::new();
+    let mut atlantic = HashSet::new();
+
+    fn dfs(
+        rows: usize,
+        cols: usize,
+        r: isize,
+        c: isize,
+        heights: &[Vec<i32>],
+        visited: &mut HashSet<(usize, usize)>,
+        prev_height: i32,
+    ) {
+        if r < 0
+            || r >= rows as isize
+            || c < 0
+            || c >= cols as isize
+            || visited.contains(&(r as usize, c as usize))
+            || heights[r as usize][c as usize] < prev_height
+        {
+            return;
+        }
+        let (r, c) = (r as usize, c as usize);
+        visited.insert((r, c));
+        let directions = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
+        for (x, y) in directions {
+            let (i, j) = (r as isize + x, c as isize + y);
+            dfs(rows, cols, i, j, heights, visited, heights[r][c]);
+        }
+    }
+
     for c in 0..cols {
         dfs(
-            rows as isize,
-            cols as isize,
-            &heights,
+            rows,
+            cols,
             0,
             c as isize,
-            &mut pac,
+            &heights,
+            &mut pacific,
             heights[0][c],
         );
         dfs(
-            rows as isize,
-            cols as isize,
-            &heights,
-            (rows - 1) as isize,
+            rows,
+            cols,
+            rows as isize - 1,
             c as isize,
-            &mut atl,
+            &heights,
+            &mut atlantic,
             heights[rows - 1][c],
         );
     }
 
     for r in 0..rows {
         dfs(
-            rows as isize,
-            cols as isize,
-            &heights,
+            rows,
+            cols,
             r as isize,
             0,
-            &mut pac,
+            &heights,
+            &mut pacific,
             heights[r][0],
         );
         dfs(
-            rows as isize,
-            cols as isize,
-            &heights,
+            rows,
+            cols,
             r as isize,
-            (cols - 1) as isize,
-            &mut atl,
+            cols as isize - 1,
+            &heights,
+            &mut atlantic,
             heights[r][cols - 1],
         );
     }
 
-    for r in 0..rows {
-        for c in 0..cols {
-            if pac.contains(&(r as isize, c as isize)) && atl.contains(&(r as isize, c as isize)) {
-                result.push(vec![r as i32, c as i32]);
+    for i in 0..rows {
+        for j in 0..cols {
+            let index = &(i, j);
+            if pacific.contains(index) && atlantic.contains(index) {
+                result.push(vec![i as i32, j as i32])
             }
         }
     }
@@ -57,87 +90,24 @@ pub fn pacific_atlantic(heights: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     result
 }
 
-fn dfs(
-    rows: isize,
-    cols: isize,
-    heights: &[Vec<i32>],
-    r: isize,
-    c: isize,
-    visited: &mut HashSet<(isize, isize)>,
-    prev_height: i32,
-) {
-    if visited.contains(&(r, c))
-        || r < 0
-        || c < 0
-        || r == rows
-        || c == cols
-        || heights[r as usize][c as usize] < prev_height
-    {
-        return;
-    }
-
-    visited.insert((r, c));
-    dfs(
-        rows,
-        cols,
-        heights,
-        r + 1,
-        c,
-        visited,
-        heights[r as usize][c as usize],
+#[test]
+fn test_pacific_atlantic() {
+    assert_eq!(
+        vec![
+            vec![0, 4],
+            vec![1, 3],
+            vec![1, 4],
+            vec![2, 2],
+            vec![3, 0],
+            vec![3, 1],
+            vec![4, 0]
+        ],
+        pacific_atlantic(vec![
+            vec![1, 2, 2, 3, 5],
+            vec![3, 2, 3, 4, 4],
+            vec![2, 4, 5, 3, 1],
+            vec![6, 7, 1, 4, 5],
+            vec![5, 1, 1, 2, 4]
+        ])
     );
-    dfs(
-        rows,
-        cols,
-        heights,
-        r - 1,
-        c,
-        visited,
-        heights[r as usize][c as usize],
-    );
-    dfs(
-        rows,
-        cols,
-        heights,
-        r,
-        c + 1,
-        visited,
-        heights[r as usize][c as usize],
-    );
-    dfs(
-        rows,
-        cols,
-        heights,
-        r,
-        c - 1,
-        visited,
-        heights[r as usize][c as usize],
-    );
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_pacific_atlantic() {
-        assert_eq!(
-            vec![
-                vec![0, 4],
-                vec![1, 3],
-                vec![1, 4],
-                vec![2, 2],
-                vec![3, 0],
-                vec![3, 1],
-                vec![4, 0]
-            ],
-            pacific_atlantic(vec![
-                vec![1, 2, 2, 3, 5],
-                vec![3, 2, 3, 4, 4],
-                vec![2, 4, 5, 3, 1],
-                vec![6, 7, 1, 4, 5],
-                vec![5, 1, 1, 2, 4]
-            ])
-        );
-    }
 }
