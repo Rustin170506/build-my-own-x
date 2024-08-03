76ba7443f0d6e17b02226c7dae0b7b2d7900eb35
hi-rustin <rustin.liu@gmail.com>
2024-08-03 14:17:33 +0800

    lr: q_0207_course_schedule.rs

--- lr/src/questions/q_0207_course_schedule.rs
+++ lr/src/questions/q_0207_course_schedule.rs
@@ -6,10 +6,10 @@ pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
         pre_map.entry(pre[0]).or_default().push(pre[1]);
     }
 
-    let mut visited = HashSet::new();
+    let mut cycle = HashSet::new();
 
     for c in 0..num_courses {
-        if !dfs(c, &mut pre_map, &mut visited) {
+        if !dfs(c, &mut pre_map, &mut cycle) {
             return false;
         }
     }
@@ -39,17 +39,12 @@ fn dfs(c: i32, pre_map: &mut HashMap<i32, Vec<i32>>, visited: &mut HashSet<i32>)
     true
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_can_finish() {
-        assert!(can_finish(2, vec![vec![1, 0]]));
-        assert!(!can_finish(2, vec![vec![1, 0], vec![0, 1]]));
-        assert!(can_finish(
-            5,
-            vec![vec![1, 4], vec![2, 4], vec![3, 1], vec![3, 2]]
-        ));
-    }
+#[test]
+fn test_can_finish() {
+    assert!(can_finish(2, vec![vec![1, 0]]));
+    assert!(!can_finish(2, vec![vec![1, 0], vec![0, 1]]));
+    assert!(can_finish(
+        5,
+        vec![vec![1, 4], vec![2, 4], vec![3, 1], vec![3, 2]]
+    ));
 }
