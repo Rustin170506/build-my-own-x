3e2081bd9eb1b1fd890d5b66d8ee189690a5d3ef
Rustin170506 <29879298+Rustin170506@users.noreply.github.com>
2024-09-11 13:00:00 +0000

    lr: q_0743_network_delay_time
    Signed-off-by: Rustin170506 <29879298+Rustin170506@users.noreply.github.com>

--- lr/src/questions/q_0743_network_delay_time.rs
+++ lr/src/questions/q_0743_network_delay_time.rs
@@ -4,40 +4,43 @@ use std::{
 };
 
 pub fn network_delay_time(times: Vec<Vec<i32>>, n: i32, k: i32) -> i32 {
-    let mut edges = HashMap::new();
-    for time in times {
+    let mut edges: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
+    times.iter().for_each(|time| {
         let source = time[0];
         let target = time[1];
         let weight = time[2];
         edges
             .entry(source)
-            .and_modify(|e: &mut Vec<(i32, i32)>| e.push((weight, target)))
+            .and_modify(|v| {
+                v.push((weight, target));
+            })
             .or_insert(vec![(weight, target)]);
-    }
+    });
 
-    let mut min_heap: BinaryHeap<Reverse<(i32, i32)>> = BinaryHeap::new();
+    let mut time = 0;
+    let mut min_heap = BinaryHeap::new();
     min_heap.push(Reverse((0, k)));
     let mut visited = HashSet::new();
-    let mut t = 0;
     while !min_heap.is_empty() {
-        let (w1, n1) = min_heap.pop().unwrap().0;
-        if visited.contains(&n1) {
+        let (weight, node) = min_heap.pop().unwrap().0;
+        if visited.contains(&node) {
             continue;
         }
-        visited.insert(n1);
-        t = t.max(w1);
+        time = time.max(weight);
+        visited.insert(node);
 
-        if let Some(edges) = edges.get(&n1) {
-            for (w2, n2) in edges.iter() {
-                if !visited.contains(n2) {
-                    min_heap.push(Reverse((w1 + w2, *n2)));
+        if let Some(edges) = edges.get(&node) {
+            for (neighbor_weight, neighbor) in edges {
+                if visited.contains(neighbor) {
+                    continue;
                 }
+                min_heap.push(Reverse((weight + neighbor_weight, *neighbor)));
             }
         }
     }
 
     if visited.len() == n as usize {
-        t
+        time
     } else {
         -1
     }
