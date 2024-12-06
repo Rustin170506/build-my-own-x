f086a5890e87326a20c307bae0ce55246973d89e
Rustin170506 <tech@rustin.me>
2024-12-06 09:37:51 +0800

    lg: q_0110
    Signed-off-by: Rustin170506 <tech@rustin.me>

--- lg/src/questions/q_0110_balanced_binary_tree.go
+++ lg/src/questions/q_0110_balanced_binary_tree.go
@@ -6,23 +6,19 @@ import (
 	"github.com/hi-rustin/lg/src/utils"
 )
 
-func isBalanced(root *utils.TreeNode) bool {
-	var helper func(root *utils.TreeNode) int
-	helper = func(node *utils.TreeNode) int {
-		if node == nil {
-			return 0
-		}
-		leftHeight := helper(node.Left)
-		rightHeight := helper(node.Right)
-		return max(leftHeight, rightHeight) + 1
+func helper(root *utils.TreeNode) float64 {
+	if root == nil {
+		return 0
 	}
+	return max(helper(root.Left), helper(root.Right)) + 1
+}
+
+func isBalanced(root *utils.TreeNode) bool {
 	if root == nil {
 		return true
 	}
 
-	leftHeight := helper(root.Left)
-	rightHeight := helper(root.Right)
-	if math.Abs(float64(leftHeight-rightHeight)) > 1 {
+	if math.Abs(helper(root.Left)-helper(root.Right)) > 1 {
 		return false
 	}
 
