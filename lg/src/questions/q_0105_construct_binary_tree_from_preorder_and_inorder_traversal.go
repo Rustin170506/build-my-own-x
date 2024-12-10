0c57d2aa2a6897a7649ad156f67403554df0e95f
Rustin170506 <tech@rustin.me>
2024-12-10 10:27:41 +0800

    lg: q_0105
    Signed-off-by: Rustin170506 <tech@rustin.me>

--- lg/src/questions/q_0105_construct_binary_tree_from_preorder_and_inorder_traversal.go
+++ lg/src/questions/q_0105_construct_binary_tree_from_preorder_and_inorder_traversal.go
@@ -8,14 +8,16 @@ func buildTree(preorder []int, inorder []int) *utils.TreeNode {
 	if len(preorder) == 0 || len(inorder) == 0 {
 		return nil
 	}
-
-	root := &utils.TreeNode{Val: preorder[0]}
+	root := &utils.TreeNode{
+		Val: preorder[0],
+	}
 	mid := 0
-	for index, n := range inorder {
-		if n == preorder[0] {
-			mid = index
+	for i, v := range inorder {
+		if v == preorder[0] {
+			mid = i
 		}
 	}
+
 	root.Left = buildTree(preorder[1:mid+1], inorder[:mid])
 	root.Right = buildTree(preorder[mid+1:], inorder[mid+1:])
 
