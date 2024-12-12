71bbf8e78a7ecb97fceac32315707269b5af8aab
Rustin170506 <tech@rustin.me>
2024-12-12 12:04:03 +0800

    lg: q_0297
    Signed-off-by: Rustin170506 <tech@rustin.me>

--- lg/src/questions/q_0297_serialize_and_deserialize_binary_tree.go
+++ lg/src/questions/q_0297_serialize_and_deserialize_binary_tree.go
@@ -17,37 +17,47 @@ func CodecConstructor() Codec {
 
 // Serializes a tree to a single string.
 func (this *Codec) serialize(root *utils.TreeNode) string {
-	result := make([]string, 0)
-	var dfs func(node *utils.TreeNode)
-	dfs = func(node *utils.TreeNode) {
-		if node == nil {
-			result = append(result, "null")
+	if root == nil {
+		return ""
+	}
+
+	strs := make([]string, 0)
+	var dfs func(root *utils.TreeNode)
+	dfs = func(root *utils.TreeNode) {
+		if root == nil {
+			strs = append(strs, "null")
 			return
 		}
-		result = append(result, fmt.Sprintf("%v", node.Val))
-		dfs(node.Left)
-		dfs(node.Right)
+		strs = append(strs, fmt.Sprintf("%d", root.Val))
+		dfs(root.Left)
+		dfs(root.Right)
 	}
+
 	dfs(root)
-	return strings.Join(result, ",")
+	return strings.Join(strs, ",")
 }
 
 // Deserializes your encoded data to tree.
 func (this *Codec) deserialize(data string) *utils.TreeNode {
-	nodes := strings.Split(data, ",")
+	if data == "" {
+		return nil
+	}
+	strs := strings.Split(data, ",")
 	i := 0
 	var dfs func() *utils.TreeNode
 	dfs = func() *utils.TreeNode {
-		nodeVal := nodes[i]
-		if nodeVal == "null" {
+		str := strs[i]
+		if str == "null" {
 			i++
 			return nil
 		}
-		val, _ := strconv.Atoi(nodeVal)
-		node := &utils.TreeNode{Val: val}
+		node := &utils.TreeNode{}
+		val, _ := strconv.Atoi(str)
+		node.Val = val
 		i++
 		node.Left = dfs()
 		node.Right = dfs()
+
 		return node
 	}
 
