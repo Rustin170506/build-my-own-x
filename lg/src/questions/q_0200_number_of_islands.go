540318875a2015bb4ae86cd1c76757a3623b3efe
Rustin170506 <tech@rustin.me>
2024-12-13 10:48:18 +0800

    lg: q_0200
    Signed-off-by: Rustin170506 <tech@rustin.me>

--- lg/src/questions/q_0200_number_of_islands.go
+++ lg/src/questions/q_0200_number_of_islands.go
@@ -6,43 +6,44 @@ func numIslands(grid [][]byte) int {
 	if len(grid) == 0 {
 		return 0
 	}
+
 	result := 0
 	visited := make(map[string]struct{})
+	directions := [][2]int{{0, -1}, {0, 1}, {1, 0}, {-1, 0}}
 	rows, cols := len(grid), len(grid[0])
-	bfs := func(i int, j int) {
-		queue := make([][]int, 0)
-		visited[fmt.Sprintf("%d:%d", i, j)] = struct{}{}
-		queue = append(queue, []int{i, j})
+	bfs := func(i, j int) {
+		queue := make([][2]int, 0)
+		visited[fmt.Sprintf("%d-%d", i, j)] = struct{}{}
+		queue = append(queue, [2]int{i, j})
 		for len(queue) > 0 {
-			curr := queue[0]
+			row, col := queue[0][0], queue[0][1]
 			queue = queue[1:]
-			i, j = curr[0], curr[1]
-			directions := [][]int{{0, -1}, {0, 1}, {1, 0}, {-1, 0}}
-			for _, d := range directions {
-				newRow := d[0] + i
-				newCol := d[1] + j
-				if newRow < rows && newRow >= 0 && newCol < cols && newCol >= 0 && grid[newRow][newCol] == '1' {
-					if _, ok := visited[fmt.Sprintf("%d:%d", newRow, newCol)]; !ok {
-						visited[fmt.Sprintf("%d:%d", newRow, newCol)] = struct{}{}
-						queue = append(queue, []int{newRow, newCol})
-					}
+			for _, direction := range directions {
+				r, c := direction[0], direction[1]
+				newRow := row + r
+				newCol := col + c
+				if newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols || grid[newRow][newCol] == '0' {
+					continue
+				}
+				idx := fmt.Sprintf("%d-%d", newRow, newCol)
+				if _, ok := visited[idx]; !ok {
+					visited[idx] = struct{}{}
+					queue = append(queue, [2]int{newRow, newCol})
 				}
 			}
 		}
-
 	}
 
 	for i := 0; i < rows; i++ {
 		for j := 0; j < cols; j++ {
 			if grid[i][j] == '1' {
-				if _, ok := visited[fmt.Sprintf("%d:%d", i, j)]; !ok {
+				if _, ok := visited[fmt.Sprintf("%d-%d", i, j)]; !ok {
 					bfs(i, j)
 					result += 1
 				}
 			}
 		}
 	}
-
 	return result
 }
 
