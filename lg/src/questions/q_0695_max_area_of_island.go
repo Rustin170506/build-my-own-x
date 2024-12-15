2d96650af8025dc5b23bcbed45e80222b7246cb2
Rustin170506 <techregister@pm.me>
2024-12-15 10:40:11 +0800

    lg: q_0695
    Signed-off-by: Rustin170506 <techregister@pm.me>

--- lg/src/questions/q_0695_max_area_of_island.go
+++ lg/src/questions/q_0695_max_area_of_island.go
@@ -1,33 +1,41 @@
 package questions
 
+import "fmt"
+
 func maxAreaOfIsland(grid [][]int) int {
+	if len(grid) == 0 {
+		return 0
+	}
 	rows := len(grid)
 	cols := len(grid[0])
-	result := 0
-
+	directions := [][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+	visited := make(map[string]struct{})
 	var dfs func(row, col int) int
 	dfs = func(row, col int) int {
 		if row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] == 0 {
 			return 0
 		}
-
-		count := 1
-		grid[row][col] = 0
-		directions := [][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
-		for _, direction := range directions {
-			count += dfs(row+direction[0], col+direction[1])
+		idx := fmt.Sprintf("%d-%d", row, col)
+		if _, ok := visited[idx]; ok {
+			return 0
 		}
 
-		return count
+		visited[idx] = struct{}{}
+		res := 1
+		for _, direction := range directions {
+			newRow, newCol := row+direction[0], col+direction[1]
+			res += dfs(newRow, newCol)
+		}
+
+		return res
 	}
 
-	for i := 0; i < rows; i++ {
-		for j := 0; j < cols; j++ {
-			if grid[i][j] == 1 {
-				count := dfs(i, j)
-				if count > result {
-					result = count
-				}
+	result := 0
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			idx := fmt.Sprintf("%d-%d", row, col)
+			if _, ok := visited[idx]; !ok && grid[row][col] == 1 {
+				result = max(result, dfs(row, col))
 			}
 		}
 	}
