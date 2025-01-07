c9143f6c9e2c005a3cd0476e7e7144f7c5c90efb
Rustin170506 <techregister@pm.me>
2025-01-07 18:37:50 +0800

    lr: q_0860
    Signed-off-by: Rustin170506 <techregister@pm.me>

---
+++ lr/src/questions/q_0860_lemonade_change.rs
@@ -0,0 +1,43 @@
+pub fn lemonade_change(bills: Vec<i32>) -> bool {
+    let (mut five, mut ten) = (0, 0);
+    for bill in bills {
+        match bill {
+            5 => five += 1,
+            10 => {
+                if five < 1 {
+                    return false;
+                }
+                ten += 1;
+                five -= 1;
+            }
+            20 => {
+                if five >= 1 && ten >= 1 {
+                    five -= 1;
+                    ten -= 1;
+                } else if five >= 3 {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    true
+}
+
+#[test]
+fn test_lemonade_change() {
+    let bills = vec![5, 5, 5, 10, 20];
+    let result = lemonade_change(bills);
+    assert_eq!(result, true);
+
+    let bills = vec![5, 5, 10];
+    let result = lemonade_change(bills);
+    assert_eq!(result, true);
+
+    let bills = vec![10, 10];
+    let result = lemonade_change(bills);
+    assert_eq!(result, false);
+}
